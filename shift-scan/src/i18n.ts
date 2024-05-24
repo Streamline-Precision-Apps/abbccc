aa0976e39deafaada9bf20c6a6d145b53c606fdd
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-05-24 09:34:18 -0600

    keep changes

--- shift-scan/src/i18n.ts
+++ shift-scan/src/i18n.ts
@@ -1,14 +1,13 @@
-import {notFound} from 'next/navigation';
+import {NextIntlClientProvider} from 'next-intl';
 import {getRequestConfig} from 'next-intl/server';
-
-// Can be imported from a shared config
-const locales = ['en', 'es'];
-
-export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
-    if (!locales.includes(locale as any)) notFound();
-
-    return {
+ 
+export default getRequestConfig(async () => {
+  // Provide a static locale, fetch a user setting,
+  // read from `cookies()`, `headers()`, etc.
+  const locale = 'en';
+ 
+  return {
+    locale,
     messages: (await import(`../messages/${locale}.json`)).default
-    };
+  };
 });
