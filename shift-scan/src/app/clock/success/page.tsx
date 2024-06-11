f5b3f94577929c1459c6ee7af8bc2511ad5ccf34
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-06-11 13:58:22 -0600

    made a beforeOnload protection to prompt users to be sure when leaving a page before hand.

--- shift-scan/src/app/clock/success/page.tsx
+++ shift-scan/src/app/clock/success/page.tsx
@@ -6,6 +6,7 @@ import '../../globals.css';
 import { useScanData } from '../../../../components/context/ScannedJobSIte';
 import { useSavedCostCode } from '../../../../components/context/SavedCostCode';
 import RedirectAfterDelay from '../../../../components/clock/redirectAfterDelay';
+import useBeforeUnload from '../../../../components/app/refreshWarning';
 
 export default function verify() {
 // this page will be used when the user has successfully clocked in
@@ -17,15 +18,21 @@ const { scanResult} = useScanData();
 const { savedCostCode} = useSavedCostCode();
 const now = new Date();
 
-// to have a delay of 5 seconds uncomment the line below
+const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+    const message = t('lN6');
+    event.returnValue = message;
+    return message;
+}
 
+// to have a delay of 5 seconds uncomment the line below
+useBeforeUnload(handleBeforeUnload);
 return (
     <div className='flex flex-col items-center '>
         <h1> {} </h1>
         <h2>{t('lN2')} {scanResult?.data}</h2>
         <h2>{t('lN3')} {savedCostCode}</h2>
         <p>{t('lN4')}</p>
-        {/* <RedirectAfterDelay delay={5000} to="/dashboard" /> */}
+        <RedirectAfterDelay delay={5000} to="/dashboard" />
     </div>
     );
 }
--- shift-scan/src/app/clock/verify/page.tsx
+++ shift-scan/src/app/clock/verify/page.tsx
@@ -7,6 +7,7 @@ import '../../globals.css';
 import { Clock } from "../../../../components/clock";
 import { useScanData } from '../../../../components/context/ScannedJobSIte';
 import { useSavedCostCode } from '../../../../components/context/SavedCostCode';
+import useBeforeUnload from '../../../../components/app/refreshWarning';
 
 export default function verify() {
 // preclocked in page confirmation to send data to api route
@@ -15,12 +16,21 @@ const [today] = useState(new Date());
 const { scanResult} = useScanData();
 const { savedCostCode} = useSavedCostCode();
 // by using usestate we can change the value of the variable later
-
 const now = new Date();
+
+const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+    const message = t('lN6');
+    event.returnValue = message;
+    return message;
+}
 // this is for the live clock page
 // provide the data such as job found and costcode
 // eventually you will not beable to access this page unless you are logged in and have been 
 // throught the costcode finder and scanned in.
+
+// this is a custom hook to use beforeunload
+useBeforeUnload(handleBeforeUnload);
+
 return (
     <div className='flex flex-col items-center '>
         <h1>{t('lN1')}</h1>
