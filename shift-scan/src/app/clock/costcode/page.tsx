cc7eff6af7fb7eb65297387ad0e6c9c00c610aac
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-06-11 13:58:22 -0600

    made a beforeOnload protection to prompt users to be sure when leaving a page before hand.

--- shift-scan/src/app/clock/costcode/page.tsx
+++ shift-scan/src/app/clock/costcode/page.tsx
@@ -1,8 +1,18 @@
-
+"use client";
 import React from 'react';
 import  CostCodeFinder from '../../../../components/clock/costcodeFInder';
+import useBeforeUnload from '../../../../components/app/refreshWarning';
+import { useTranslations } from 'next-intl';
+
 
 const CostCodePage: React.FC = () => {
+  const t = useTranslations('page5');
+  const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+    const message = t('lN6');
+    event.returnValue = message;
+    return message;
+  }
+  useBeforeUnload(handleBeforeUnload);
   return (
     <div>
       < CostCodeFinder />
