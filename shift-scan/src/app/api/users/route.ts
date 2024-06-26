7ab8fae81c8e0680e966eadba1a1381658924e2b
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-06-26 11:18:24 -0600

    all of the employee api routes are used now!

--- shift-scan/src/app/api/users/route.ts
+++ shift-scan/src/app/api/users/route.ts
@@ -1,30 +1,26 @@
 // this is a rest api endpoint
 import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-    const body = await req.json()
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json();
 
-    const result = await prisma.user.create({
-        data: {
-            ...body
-        }
-    })
-    if (!result) return Response.json({
-        message: 'error',
-        status: 500
-    })
-    return Response.json({
-        message: 'success',
-        status: 200,
-        data: result
-    })
+        const created = await prisma.user.create({
+            ...body 
+        });
+
+        return new NextResponse(JSON.stringify(created));
+
+    } catch (error) {
+        // Log the error for debugging
+        console.error('Failed to create user:', error);
+    }
 }
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
     const result = await prisma.user.findMany()
-    return Response.json({
-        message: 'ok',
-        status: 200,
-        data: result
-    })
-}
+    return NextResponse.json(result)
+    }
+
