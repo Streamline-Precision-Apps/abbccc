baf8cb8897ef6dbd12b2be5ab55d85947f7db678
Sean Walker <Sbsbw@byui.edu>
2024-07-08 10:25:28 -0600

    fixed merge conflicts

--- shift-scan/src/app/providers.tsx
+++ shift-scan/src/app/providers.tsx
@@ -5,21 +5,22 @@ import { ScanDataProvider } from "./context/JobSiteContext";
 import { SavedCostCodeProvider } from "./context/CostCodeContext";
 import { SessionProvider } from "next-auth/react";
 import { SavedPayPeriodHoursProvider } from "./context/SavedPayPeriodHours";
-import { SavedUserDataProvider } from "./context/UserContext";import { SavedUserDataProvider } from './context/UserContext';
-
+import { SavedUserDataProvider } from "./context/UserContext";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-            <SavedUserDataProvider>      <SavedUserDataProvider>
-        <SavedPayPeriodHoursProvider>
-          <SavedCostCodeProvider>
-            <ScanDataProvider>
-              <SessionProvider>{children}</SessionProvider>
-            </ScanDataProvider>
-          </SavedCostCodeProvider>
-        </SavedPayPeriodHoursProvider>
-            </SavedUserDataProvider>
+      <SavedUserDataProvider>
+        {" "}
+        <SavedUserDataProvider>
+          <SavedPayPeriodHoursProvider>
+            <SavedCostCodeProvider>
+              <ScanDataProvider>
+                <SessionProvider>{children}</SessionProvider>
+              </ScanDataProvider>
+            </SavedCostCodeProvider>
+          </SavedPayPeriodHoursProvider>
+        </SavedUserDataProvider>
       </SavedUserDataProvider>
     </>
   );
