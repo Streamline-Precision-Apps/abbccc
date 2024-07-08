1d81e1a7567525b086ecd32cfbd09b5ee887a5ef
Sean Walker <Sbsbw@byui.edu>
2024-07-08 10:05:59 -0600

    fix my merge issues

--- shift-scan/src/app/providers.tsx
+++ shift-scan/src/app/providers.tsx
@@ -1,24 +1,27 @@
-"use client";
-// this is the provider for the app, it wraps all the components
+'use client';
+// this is the provider for the app, it wraps all the components 
 // this is so that all the components can access the context
-import { ScanDataProvider } from "./context/JobSiteContext";
-import { SavedCostCodeProvider } from "./context/CostCodeContext";
-import { SessionProvider } from "next-auth/react";
-import { SavedPayPeriodHoursProvider } from "./context/SavedPayPeriodHours";
-import { SavedUserDataProvider } from "./context/UserContext";
+import {ScanDataProvider} from './context/JobSiteContext';
+import {SavedCostCodeProvider} from './context/CostCodeContext';
+import {SessionProvider} from 'next-auth/react';
+import { SavedPayPeriodHoursProvider } from './context/SavedPayPeriodHours';
+import { SavedUserDataProvider } from './context/UserContext';
+
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <SavedUserDataProvider>
-        <SavedPayPeriodHoursProvider>
-          <SavedCostCodeProvider>
+    return <>
+            <SavedUserDataProvider>
+            <SavedPayPeriodHoursProvider>
+            <SavedCostCodeProvider>
             <ScanDataProvider>
-              <SessionProvider>{children}</SessionProvider>
+            <SessionProvider>
+                {children}
+            </SessionProvider>
             </ScanDataProvider>
-          </SavedCostCodeProvider>
-        </SavedPayPeriodHoursProvider>
-      </SavedUserDataProvider>
-    </>
-  );
+            </SavedCostCodeProvider>
+            </SavedPayPeriodHoursProvider>
+            </SavedUserDataProvider>
+
+            
+        </>;
 }
