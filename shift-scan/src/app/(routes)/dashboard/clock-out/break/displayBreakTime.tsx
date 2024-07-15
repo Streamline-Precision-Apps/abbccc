1ab1ade896330edab5b1a55cb3d9c39b40b57d69
Sean Walker <Sbsbw@byui.edu>
2024-07-15 14:09:33 -0600

    Fixed Break reset and breaktimecontext.

--- shift-scan/src/app/(routes)/dashboard/clock-out/break/displayBreakTime.tsx
+++ shift-scan/src/app/(routes)/dashboard/clock-out/break/displayBreakTime.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useTranslations } from "next-intl";
-import { useEffect } from "react";
 import ViewHoursComponent from "@/app/(content)/hoursControl";
 import { useSavedBreakTime } from "@/app/context/SavedBreakTimeContext";
+import { useEffect } from "react";
 
 interface BreakTimeProps {
   display: boolean;
@@ -16,22 +16,30 @@ export default function DisplayBreakTime({
   const t = useTranslations("page1");
   const { breakTime, setBreakTime } = useSavedBreakTime();
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setBreakTime((prevBreakTime: number) => prevBreakTime + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [setBreakTime]);
-
   const handler = () => {
     setToggle(!display);
   };
 
-  const formatTime = (time: number) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  useEffect(() => {
+    let timer: NodeJS.Timeout | undefined;
+
+    if (display) {
+      timer = setInterval(() => {
+        setBreakTime((prevBreakTime) => prevBreakTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [display, setBreakTime]);
+
+  const formatTime = (seconds: number) => {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins}m ${secs}s`;
   };
 
   return display ? (
@@ -41,7 +49,7 @@ export default function DisplayBreakTime({
         className="mb-2 flex justify-center m-auto items-center space-x-12 w-11/12 h-36 text-white bg-app-dark-blue rounded-lg lg:space-x-12 lg:h-20"
       >
         <h2 className="text-4xl">Break Time: </h2>
-        <span className="w-1/4 bg-white text-2xl text-black py-3 px-2 rounded border-2 border-black lg:text-2xl lg:p-3">
+        <span className="w-1/4 bg-white text-2xl text-black py-3 px-2 rounded border-2 border-black rounded-2xl lg:text-2xl lg:p-3">
           {formatTime(breakTime)}
         </span>
       </button>
