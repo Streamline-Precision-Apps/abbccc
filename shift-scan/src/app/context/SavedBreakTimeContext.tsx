613480d034a11c7511f47a27385294ced6937665
Sean Walker <Sbsbw@byui.edu>
2024-07-15 14:09:33 -0600

    Fixed Break reset and breaktimecontext.

--- shift-scan/src/app/context/SavedBreakTimeContext.tsx
+++ shift-scan/src/app/context/SavedBreakTimeContext.tsx
@@ -1,34 +1,15 @@
 "use client";
-import React, {
-  createContext,
-  useState,
-  useEffect,
-  ReactNode,
-  useContext,
-} from "react";
+import React, { createContext, useState, useContext, ReactNode, useEffect } from "react";
 
 interface SavedBreakTimeProps {
   breakTime: number;
   setBreakTime: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const SavedBreakTimeContext = createContext<SavedBreakTimeProps | undefined>(
-  undefined
-);
+const SavedBreakTimeContext = createContext<SavedBreakTimeProps | undefined>(undefined);
 
-export const SavedBreakTimeProvider: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => {
-  const [breakTime, setBreakTime] = useState<number>(() => {
-    // Initialize breakTime from localStorage if it exists, otherwise default to 0
-    const savedBreakTime = localStorage.getItem("breakTime");
-    return savedBreakTime ? parseInt(savedBreakTime, 10) : 0;
-  });
-
-  useEffect(() => {
-    // Save breakTime to localStorage whenever it changes
-    localStorage.setItem("breakTime", breakTime.toString());
-  }, [breakTime]);
+export const SavedBreakTimeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [breakTime, setBreakTime] = useState<number>(0);
 
   return (
     <SavedBreakTimeContext.Provider value={{ breakTime, setBreakTime }}>
@@ -40,9 +21,7 @@ export const SavedBreakTimeProvider: React.FC<{ children: ReactNode }> = ({
 export const useSavedBreakTime = () => {
   const context = useContext(SavedBreakTimeContext);
   if (context === undefined) {
-    throw new Error(
-      "useSavedBreakTime must be used within a SavedBreakTimeProvider"
-    );
+    throw new Error("useSavedBreakTime must be used within a SavedBreakTimeProvider");
   }
   return context;
 };
