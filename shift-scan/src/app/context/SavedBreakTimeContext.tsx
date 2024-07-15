18bcd309310160942dd2c63b664241f1d761fe1c
Sean Walker <Sbsbw@byui.edu>
2024-07-15 11:26:23 -0600

    Finished cleaning up break page.

--- shift-scan/src/app/context/SavedBreakTimeContext.tsx
+++ shift-scan/src/app/context/SavedBreakTimeContext.tsx
@@ -1,39 +1,47 @@
 "use client";
-import React, { createContext, useState, ReactNode, useContext } from "react";
+import React, {
+  createContext,
+  useState,
+  useEffect,
+  ReactNode,
+  useContext,
+} from "react";
 
 interface SavedBreakTimeProps {
-  breakTime: number | null;
-  setBreakTime: (breakTime: number | null) => void;
+  breakTime: number;
+  setBreakTime: React.Dispatch<React.SetStateAction<number>>;
 }
 
-interface SavedBreakTime {
-  data: number;
-}
-
-const SavedBreakTime = createContext<SavedBreakTimeProps | undefined>(
+const SavedBreakTimeContext = createContext<SavedBreakTimeProps | undefined>(
   undefined
 );
 
 export const SavedBreakTimeProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  // creates a state
-  const [breakTime, setBreakTime] = useState<number | null>(null);
-  // when the provider is called it will return the value below
+  const [breakTime, setBreakTime] = useState<number>(() => {
+    // Initialize breakTime from localStorage if it exists, otherwise default to 0
+    const savedBreakTime = localStorage.getItem("breakTime");
+    return savedBreakTime ? parseInt(savedBreakTime, 10) : 0;
+  });
+
+  useEffect(() => {
+    // Save breakTime to localStorage whenever it changes
+    localStorage.setItem("breakTime", breakTime.toString());
+  }, [breakTime]);
+
   return (
-    <SavedBreakTime.Provider
-      value={{ breakTime: breakTime, setBreakTime: setBreakTime }}
-    >
+    <SavedBreakTimeContext.Provider value={{ breakTime, setBreakTime }}>
       {children}
-    </SavedBreakTime.Provider>
+    </SavedBreakTimeContext.Provider>
   );
 };
-// this is used to get the value
+
 export const useSavedBreakTime = () => {
-  const context = useContext(SavedBreakTime);
+  const context = useContext(SavedBreakTimeContext);
   if (context === undefined) {
     throw new Error(
-      "SavedBreakTime must be used within a SavedBreakTimeProvider"
+      "useSavedBreakTime must be used within a SavedBreakTimeProvider"
     );
   }
   return context;
