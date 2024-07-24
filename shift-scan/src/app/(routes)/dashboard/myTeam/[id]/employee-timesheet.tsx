072da57d3735aabfb442c19c9021c898776eb56f
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-07-24 11:20:16 -0600

    fixed the update annoying page lag.

--- shift-scan/src/app/(routes)/dashboard/myTeam/[id]/employee-timesheet.tsx
+++ shift-scan/src/app/(routes)/dashboard/myTeam/[id]/employee-timesheet.tsx
@@ -1,43 +1,30 @@
+// employee-timesheet.tsx
 "use client";
-import "@/app/globals.css";
-import {fetchTimesheets,editTimeSheet } from "@/actions/timeSheetActions";
-import { Form } from "./form";
+
 import { useState } from "react";
+import { fetchTimesheets} from "@/actions/timeSheetActions";
+import { Form } from "./form";
 import EditButton from "@/components/editButton";
 import EditWork from "./edit-work";
 
 export const EmployeeTimeSheets = ({ employeeId }: { employeeId: string }) => {
-const [timesheets, setTimesheets] = useState<any[]>([]);
-const [edit, setEdit] = useState(false);
+    const [timesheets, setTimesheets] = useState<any[]>([]);
+    const [edit, setEdit] = useState(false);
 
+    const handleFormSubmitWrapper = async (date: string) => {
+        const results = await fetchTimesheets(employeeId, date);
+        setTimesheets(results);
+    };
 
-const handleFormSubmitWrapper = async (date: string) => {
-const results = await fetchTimesheets(employeeId, date);
-    setTimesheets(results);
-};
+    return (
+        <div className="mx-auto h-auto w-full lg:w-1/2 flex flex-col justify-center bg-gradient-to-b from-app-dark-blue via-app-dark-blue to-app-blue py-20 border-l-2 border-r-2 border-black">
+        <div className="flex flex-col py-5 px-2 w-11/12 mx-auto h-1/4 border-2 border-black rounded-2xl text-white text-3xl">
+            <h1 className="text-2xl pl-5 lg:text-3xl mb-5">Select Date</h1>
+            <Form employeeId={employeeId} onFormSubmit={handleFormSubmitWrapper} />
+            <EditButton edit={edit} setEdit={setEdit} />
+            <EditWork timesheetData={timesheets} edit={edit} />
+        </div>
+        </div>
+    );
+    };
 
-
-return edit ? (
-<div className="mx-auto h-auto w-full lg:w-1/2 flex flex-col justify-center bg-gradient-to-b from-app-dark-blue via-app-dark-blue to-app-blue py-20 border-l-2 border-r-2 border-black">
-<div className="flex flex-col py-5 px-2 w-11/12 mx-auto h-1/4 border-2 border-black rounded-2xl text-white text-3xl">
-    <h1 className="text-2xl pl-5 lg:text-3xl mb-5">Select Date</h1>
-    <Form employeeId={employeeId} onFormSubmit={handleFormSubmitWrapper} />
-
-    {/* Editable form data */}
-    <ul>
-    <EditButton edit={edit} setEdit={setEdit} />
-    <EditWork employeeId={employeeId} timesheets={timesheets} edit={edit}  />
-    </ul>
-</div>
-</div>
-) : (
-<div className="mx-auto h-auto w-full lg:w-1/2 flex flex-col justify-center bg-gradient-to-b from-app-dark-blue via-app-dark-blue to-app-blue py-20 border-l-2 border-r-2 border-black">
-<div className="flex flex-col py-5 px-2 w-11/12 mx-auto h-1/4 border-2 border-black rounded-2xl text-white text-3xl">
-    <h1 className="text-2xl pl-5 lg:text-3xl mb-5">Select Date</h1>
-    <Form employeeId={employeeId} onFormSubmit={handleFormSubmitWrapper} />
-    <EditButton edit={edit} setEdit={setEdit} />
-    <EditWork employeeId={employeeId} timesheets={timesheets} edit={edit}  />
-</div>
-</div>
-);
-};
--- shift-scan/src/app/(routes)/dashboard/myTeam/[id]/page.tsx
+++ shift-scan/src/app/(routes)/dashboard/myTeam/[id]/page.tsx
@@ -3,7 +3,7 @@ import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import EmployeeInfo from "./employee-info";
 import {EmployeeTimeSheets} from "../[id]/employee-timesheet";
 
-export default async function crewMember({params} : Params) {
+export default async function crewMember({ params }: { params: Params }) {
 
     return (
         <>
