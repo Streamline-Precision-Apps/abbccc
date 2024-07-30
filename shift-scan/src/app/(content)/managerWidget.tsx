486cdb3279ebf2fc94b8e423065eb3e5472918c1
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-07-30 14:57:59 -0600

    made necessary changes to the home scree to look visually apealing and look into the code on dash board and changed it to be changing dynamically too.

--- shift-scan/src/app/(content)/managerWidget.tsx
+++ shift-scan/src/app/(content)/managerWidget.tsx
@@ -1,3 +1,6 @@
+import { Buttons } from "@/components/(reusable)/buttons";
+import { Images } from "@/components/(reusable)/images";
+import { Texts } from "@/components/(reusable)/texts";
 import { User } from "@/lib/types";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
@@ -10,13 +13,6 @@ export default function ManagerWidget({ user }: Props) {
   const t = useTranslations("page1");
   const router = useRouter();
 
-  const loadTeamPage = () => {
-    router.push("/dashboard/myTeam");
-  };
-  const loadGeneratorPage = () => {
-    router.push("/dashboard/qr-generator");
-  };
-
   if (
     user.permission === "ADMIN" ||
     user.permission === "SUPERADMIN" ||
@@ -24,20 +20,17 @@ export default function ManagerWidget({ user }: Props) {
     user.permission === "PROJECTMANAGER"
   ) {
     return (
-      <div className="ml-10 mr-10 mb-5 mt-5 flex flex-row space-x-5 w-full h-64">
-        <button
-          className="flex justify-center items-center bg-app-blue text-black rounded-lg w-1/2 h-full border-2 border-black text-2xl"
-          onClick={loadGeneratorPage}
-        >
-          {t("lN5")}
-        </button>
-        <button
-          className="flex justify-center items-center bg-app-blue text-black rounded-lg w-1/2 h-full border-2 border-black text-2xl"
-          onClick={loadTeamPage}
-        >
-          {t("lN6")}
-        </button>
-      </div>
+    < div className="grid grid-cols-2 gap-4 w-full ">
+        <Buttons href="/dashboard/myTeam" variant={"default"} size={"widgetSm"}>
+        <Images titleImg="/myTeam.svg" titleImgAlt="my team" variant={"icon"} size={"widgetSm"}></Images>
+        <Texts>{t("lN6")}</Texts>
+      </Buttons>
+
+      <Buttons href="/dashboard/qr-generator" variant={"default"} size={"widgetSm"}>
+        <Images titleImg="/qrCode.svg" titleImgAlt="QR Code" variant={"icon"} size={"widgetSm"}></Images>
+        <Texts>{t("lN5")}</Texts>
+      </Buttons>
+    </div>
     );
   } else {
     return <></>;
--- shift-scan/src/components/header.tsx
+++ shift-scan/src/components/header.tsx
@@ -2,9 +2,8 @@ import TestingComponents from "./testingComponents";
 
 export const Header = () => {
     return (
-        <div className="flex flex-row justify-between items-center p-4 bg-gray-200 h-24 lg:h-10">
+        <div className=" bg-app-blue">
             <TestingComponents />
-            <h2 className="text-3xl">=</h2>
         </div>
     );
 }
