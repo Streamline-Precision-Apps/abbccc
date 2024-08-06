da7e5f0700fe31e48707155123d5682eb7c39d60
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-08-06 15:41:34 -0600

    made a dynamic button section for hamburger, saves the state, cancels the action if you dont like it. Only will change if you click save changes.... This is themodal option.

--- shift-scan/src/app/hamburger/settings/page.tsx
+++ shift-scan/src/app/hamburger/settings/page.tsx
@@ -3,20 +3,47 @@ import Index from "@/app/hamburger/settings/content";
 import prisma from "@/lib/prisma";
 import { cookies } from "next/headers";
 
+type Data = {
+language: string;
+approvedRequests: boolean;
+timeoffRequests: boolean;
+GeneralReminders: boolean;
+Biometric: boolean;
+cameraAccess: boolean;
+LocationAccess: boolean;
+};
+
 export default async function Settings() {
-    const u = cookies().get("user");
-    const user = u ? JSON.parse(u.value) : null;
-    const userId = user ? user.id : null;
+const u = cookies().get("user");
+const userId = u
 
-    const data = prisma.userSettings.findMany({
-        where: {
-            userId : userId
-        }
-    })
+if (!userId) {
+return <div>No user found</div>;
+}
 
-    return (
-        <div>
-            <Index data={data} />
-        </div>
-    )
+const data = await prisma.userSettings.findUnique({
+where: {
+    userId: userId.value,
+},
+select: {
+    userId: true,
+    language: true,
+    approvedRequests: true,
+    timeoffRequests: true,
+    GeneralReminders: true,
+    Biometric: true,
+    cameraAccess: true,
+    LocationAccess: true,
+},
+});
+
+if (!data) {
+return <div>No settings found for this user</div>;
+}
+
+return (
+<div>
+    <Index data={data} />
+</div>
+);
 }
