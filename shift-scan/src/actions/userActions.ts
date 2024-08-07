b752be2c5dad763c7f6f4356b49cf7ea6e981399
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-08-07 15:43:41 -0600

    applied the base 64 encoder and got the db to upload images to the app without any yelling

--- shift-scan/src/actions/userActions.ts
+++ shift-scan/src/actions/userActions.ts
@@ -1,54 +1,53 @@
 "use server";
 import prisma from "@/lib/prisma";
-import { permission } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
-export async function createUser(formData: FormData) {
-try{
-    await prisma.user.create({
-        data: {
-            firstName: formData.get('firstName') as string,
-            lastName: formData.get('lastName') as string,
-            username: formData.get('username') as string,
-            password: formData.get('password') as string,
-            DOB: formData.get('DOB') as string,
-            truck_view: (formData.get('truck_view') as unknown as boolean),
-            tasco_view: (formData.get('tasco_view')as unknown  as boolean),
-            labor_view: (formData.get('labor_view')as unknown  as boolean),
-            mechanic_view: (formData.get('mechanic_view') as unknown as boolean),
-            permission: formData.get('permission') as permission,
-            email: formData.get('email') as string,
-            emailVerified: formData.get('emailVerified') as string,
-            phone: formData.get('phone') as string,
-            image: formData.get('image') as string,
-        }
-    });
-} catch(error){
-    console.log(error);
-}
-    revalidatePath('/');
-}
+// export async function createUser(formData: FormData) {
+// try{
+//     await prisma.user.create({
+//         data: {
+//             firstName: formData.get('firstName') as string,
+//             lastName: formData.get('lastName') as string,
+//             username: formData.get('username') as string,
+//             password: formData.get('password') as string,
+//             DOB: formData.get('DOB') as string,
+//             truck_view: (formData.get('truck_view') as unknown as boolean),
+//             tasco_view: (formData.get('tasco_view')as unknown  as boolean),
+//             labor_view: (formData.get('labor_view')as unknown  as boolean),
+//             mechanic_view: (formData.get('mechanic_view') as unknown as boolean),
+//             permission: formData.get('permission') as permission,
+//             email: formData.get('email') as string,
+//             emailVerified: formData.get('emailVerified') as string,
+//             phone: formData.get('phone') as string,
+//             image: formData.get('image') as string,
+//         }
+//     });
+// } catch(error){
+//     console.log(error);
+// }
+//     revalidatePath('/');
+// }
 
-export async function updateUser(formData: FormData, id: string) {
-    await prisma.user.update({
-        where: { id },
-        data: {
-            firstName: formData.get('firstName') as string,
-            lastName: formData.get('lastName') as string,
-            username: formData.get('username') as string,
-            password: formData.get('password') as string,
-            truck_view: (formData.get('truck_view') as unknown as boolean),
-            tasco_view: (formData.get('tasco_view')as unknown  as boolean),
-            labor_view: (formData.get('labor_view')as unknown  as boolean),
-            mechanic_view: (formData.get('mechanic_view') as unknown as boolean),
-            permission: formData.get('permission') as permission,
-            email: formData.get('email') as string,
-            emailVerified: formData.get('emailVerified') as string,
-            phone: formData.get('phone') as string,
-            image: formData.get('image') as string,
-        }
-    });
-}
+// export async function updateUser(formData: FormData, id: string) {
+//     await prisma.user.update({
+//         where: { id },
+//         data: {
+//             firstName: formData.get('firstName') as string,
+//             lastName: formData.get('lastName') as string,
+//             username: formData.get('username') as string,
+//             password: formData.get('password') as string,
+//             truck_view: (formData.get('truck_view') as unknown as boolean),
+//             tasco_view: (formData.get('tasco_view')as unknown  as boolean),
+//             labor_view: (formData.get('labor_view')as unknown  as boolean),
+//             mechanic_view: (formData.get('mechanic_view') as unknown as boolean),
+//             permission: formData.get('permission') as permission,
+//             email: formData.get('email') as string,
+//             emailVerified: formData.get('emailVerified') as string,
+//             phone: formData.get('phone') as string,
+//             image: formData.get('image') as string,
+//         }
+//     });
+// }
 
 export async function deleteUser(id: string) {
     await prisma.user.delete({
@@ -57,4 +56,13 @@ export async function deleteUser(id: string) {
 }
 
 
-// we call these function by using a form action={...} in the form ... = post, delete, put, patch
+export async function uploadImage(formdata: FormData) {
+    console.log(formdata);
+    await prisma.user.update({
+        where: { id: formdata.get('id') as string },
+        data: {
+            image: formdata.get('image') as string
+        }
+    })
+
+}
