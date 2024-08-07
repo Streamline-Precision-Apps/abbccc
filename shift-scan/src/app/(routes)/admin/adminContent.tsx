490f5f9f677c5b0fe4b6780d09ee6d59ae32e79c
Sean Walker <Sbsbw@byui.edu>
2024-08-07 08:37:58 -0600

    push the start of the ADMIN

---
+++ shift-scan/src/app/(routes)/admin/adminContent.tsx
@@ -0,0 +1,94 @@
+"use client";
+import { useTranslations } from "next-intl";
+import "@/app/globals.css";
+import DashboardButtons from "@/components/dashboard-buttons";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Bases } from "@/components/(reusable)/bases";
+import { Sections } from "@/components/(reusable)/sections";
+import { Buttons } from "@/components/(reusable)/buttons";
+import { Titles } from "@/components/(reusable)/titles";
+import { Images } from "@/components/(reusable)/images";
+import { Modals } from "@/components/(reusable)/modals";
+import { Headers } from "@/components/(reusable)/headers";
+import { Banners } from "@/components/(reusable)/banners";
+import { Texts } from "@/components/(reusable)/texts";
+import { Footers } from "@/components/(reusable)/footers";
+import {
+getAuthStep,
+setAuthStep,
+isDashboardAuthenticated,
+} from "@/app/api/auth";
+import { CustomSession, User } from "@/lib/types";
+import { useSession } from "next-auth/react";
+import { useSavedUserData } from "@/app/context/UserContext";
+import AdminButtons from "@/components/adminButtons";
+
+interface AdminContentProps {
+permission: string | undefined;
+}
+
+export default function AdminContent({ permission }: AdminContentProps) {
+const t = useTranslations("admin");
+const router = useRouter();
+const [user, setData] = useState<User>({
+    id: "",
+    name: "",
+    firstName: "",
+    lastName: "",
+    permission: "",
+});
+const { data: session } = useSession() as { data: CustomSession | null };
+const { setSavedUserData } = useSavedUserData();
+const date = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    weekday: "long",
+});
+useEffect(() => {
+    if (permission !== "ADMIN" && permission !== "SUPERADMIN") {
+    router.push("/"); // Redirect to login page if not authenticated
+    } else {
+    setAuthStep("ADMIN");
+    }
+}, []);
+
+useEffect(() => {
+    if (session && session.user) {
+    setSavedUserData({
+        id: session.user.id,
+    });
+    setData({
+        id: session.user.id,
+        name: session.user.name,
+        firstName: session.user.firstName,
+        lastName: session.user.lastName,
+        permission: session.user.permission,
+    });
+    }
+}, [session]);
+
+return session ? (
+    <Bases variant={"default"} size={"default"}>
+    <Sections size={"default"}>
+        <Headers variant={"relative"} size={"default"}></Headers>
+        <Banners variant={"default"} size={"default"}>
+        <Titles variant={"default"} size={"h1"}>
+            {t("Banner")}
+        </Titles>
+        <Texts variant={"default"} size={"p1"}>
+            {date}
+        </Texts>
+        </Banners>
+        <Texts variant={"name"} size={"p1"}>
+        {t("Name", { firstName: user.firstName, lastName: user.lastName })}
+        </Texts>
+        <AdminButtons />
+        <Footers>{t("lN1")}</Footers>
+    </Sections>
+    </Bases>
+) : (
+    <></>
+);
+}
---
+++ shift-scan/src/components/adminButtons.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useTranslations } from "next-intl";
+import "@/app/globals.css";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { CustomSession } from "@/lib/types";
+import { Manager } from "@/app/(routes)/dashboard/manager";
+import { useState } from "react";
+import { Admin } from "@/app/(routes)/admin/Admin";
+
+interface AdminButtonsProps {
+}
+
+export default function AdminButtons({}: AdminButtonsProps) {
+  const t = useTranslations("admin");
+  const router = useRouter();
+  const { data: session } = useSession() as { data: CustomSession | null };
+  const user = session?.user;
+  const [additionalButtonsType, setAdditionalButtonsType] = useState<
+    string | null
+  >(null);
+
+  const handleShowManagerButtons = () => {
+    setAdditionalButtonsType(null);
+  };
+
+  const handleShowAdditionalButtons = (type: string) => {
+    setAdditionalButtonsType(type);
+  };
+  return (
+    <div className="grid grid-cols-2 grid-rows-2 gap-4 w-full m-4 p-4">
+      <Manager show={!additionalButtonsType} />
+      <Admin
+        additionalButtonsType={additionalButtonsType}
+        handleShowManagerButtons={handleShowManagerButtons}
+        handleShowAdditionalButtons={handleShowAdditionalButtons}
+      />
+    </div>
+  );
+}
