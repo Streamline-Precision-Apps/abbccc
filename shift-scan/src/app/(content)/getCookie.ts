1f4015dbbb8b1633cc51b3239713b43275839294
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-08-07 15:03:10 -0600

    made changes and made some fixes to previous work/ styles

--- shift-scan/src/app/(content)/getCookie.ts
+++ shift-scan/src/app/(content)/getCookie.ts
@@ -1,3 +1,4 @@
+// enables the use of cookies in a client side component splitting the value of a cookie into its name and returns the value
 export const getCookieValue = (name: string) => {
     const cookies = document.cookie.split('; ');
     for (let i = 0; i < cookies.length; i++) {
