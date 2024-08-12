ac1cbfcd623a7810bc9c2e75a6bde7af3deb9fa0
Zach Robker <168474023+zcrobker@users.noreply.github.com>
2024-08-12 14:44:55 -0400

    Updated sytles for myteam section

--- shift-scan/src/components/(reusable)/labels.tsx
+++ shift-scan/src/components/(reusable)/labels.tsx
@@ -19,9 +19,9 @@ const LabelVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-white",
+        default: "bg-none",
         float: "peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6", 
-        red: "bg-red-500",
+        white: "text-white",
       },
       size: {
         default: "",
