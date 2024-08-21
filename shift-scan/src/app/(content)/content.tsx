3fab443b46ddafb6fb60cdc56a4684b931756754
Sean Walker <Sbsbw@byui.edu>
2024-08-21 08:00:36 -0600

    fix clockin bug.

--- shift-scan/src/app/(content)/content.tsx
+++ shift-scan/src/app/(content)/content.tsx
@@ -4,7 +4,6 @@ import { useTranslations } from "next-intl";
 import Hours from "@/app/(content)/hours";
 import WidgetSection from "@/components/widgetSection";
 import { useSession } from "next-auth/react";
-import { CustomSession, User, PayPeriodTimesheets } from "@/lib/types";
 import { useEffect, useState, useMemo } from "react";
 import { useSavedPayPeriodHours } from "../context/SavedPayPeriodHours";
 import { useSavedUserData } from "../context/UserContext";
@@ -19,15 +18,27 @@ import { Sections } from "@/components/(reusable)/sections";
 import { Bases } from "@/components/(reusable)/bases";
 import { Header } from "@/components/header";
 import { Headers } from "@/components/(reusable)/headers";
-import { useDBJobsite, useDBCostcode, useDBEquipment } from "@/app/context/dbCodeContext";
-import { useRecentDBJobsite, useRecentDBCostcode, useRecentDBEquipment} from "@/app/context/dbRecentCodesContext";
+import {
+  useDBJobsite,
+  useDBCostcode,
+  useDBEquipment,
+} from "@/app/context/dbCodeContext";
+import {
+  useRecentDBJobsite,
+  useRecentDBCostcode,
+  useRecentDBEquipment,
+} from "@/app/context/dbRecentCodesContext";
 import { useSavedPayPeriodTimeSheet } from "../context/SavedPayPeriodTimeSheets";
-import { clockProcessProps, jobCodes, CostCode, Equipment, TimeSheets } from "@/lib/content";
+import {
+  clockProcessProps,
+  TimeSheets,
+} from "@/lib/content";
 import { Contents } from "@/components/(reusable)/contents";
 import { Grids } from "@/components/(reusable)/grids";
-
+import { User } from "@/lib/types";
 
 export default function Content({
+  session,
   locale,
   equipment,
   jobCodes,
@@ -39,7 +50,6 @@ export default function Content({
 }: clockProcessProps) {
   const t = useTranslations("Home");
   const f = useTranslations("Footer");
-  const { data: session } = useSession() as { data: CustomSession | null };
   const { setPayPeriodHours } = useSavedPayPeriodHours();
   const [toggle, setToggle] = useState(true);
   const { setSavedUserData } = useSavedUserData();
@@ -52,50 +62,58 @@ export default function Content({
     weekday: "long",
   });
   const [user, setData] = useState<User>({
-    id: "", name: "", firstName: "", lastName: "", permission: "",
+    id: "",
+    name: "",
+    firstName: "",
+    lastName: "",
+    permission: "",
   });
   const { jobsiteResults, setJobsiteResults } = useDBJobsite();
   const {
     recentlyUsedJobCodes,
     setRecentlyUsedJobCodes,
     addRecentlyUsedJobCode,
-    } = useRecentDBJobsite();
+  } = useRecentDBJobsite();
   const { costcodeResults, setCostcodeResults } = useDBCostcode();
   const {
     recentlyUsedCostCodes,
     setRecentlyUsedCostCodes,
     addRecentlyUsedCostCode,
-    } = useRecentDBCostcode();
+  } = useRecentDBCostcode();
   const { equipmentResults, setEquipmentResults } = useDBEquipment();
   const {
     recentlyUsedEquipment,
     setRecentlyUsedEquipment,
     addRecentlyUsedEquipment,
-    } = useRecentDBEquipment();
+  } = useRecentDBEquipment();
 
-  const { setPayPeriodTimeSheets } = useSavedPayPeriodTimeSheet(); // Correct name here
-  setPayPeriodTimeSheets(payPeriodSheets); // Correct usage here
+  const { setPayPeriodTimeSheets } = useSavedPayPeriodTimeSheet();
+
+  useEffect(() => {
+    setPayPeriodTimeSheets(payPeriodSheets);
+  }, [payPeriodSheets, setPayPeriodTimeSheets]);
 
   useEffect(() => {
     if (jobsiteResults.length === 0) {
-        setJobsiteResults(jobCodes);
+      setJobsiteResults(jobCodes);
     }
     if (costcodeResults.length === 0) {
-        setCostcodeResults(CostCodes);
+      setCostcodeResults(CostCodes);
     }
     if (equipmentResults.length === 0) {
-        setEquipmentResults(equipment);
+      setEquipmentResults(equipment);
     }
     if (recentlyUsedJobCodes.length === 0) {
-        setRecentlyUsedJobCodes(recentJobSites);
+      setRecentlyUsedJobCodes(recentJobSites);
     }
     if (recentlyUsedCostCodes.length === 0) {
-        setRecentlyUsedCostCodes(recentCostCodes);
+      setRecentlyUsedCostCodes(recentCostCodes);
     }
     if (recentlyUsedEquipment.length === 0) {
-        setRecentlyUsedEquipment(recentEquipment);
+      setRecentlyUsedEquipment(recentEquipment);
     }
-    }, [
+  }, [
+    session,
     jobCodes,
     CostCodes,
     equipment,
@@ -108,10 +126,8 @@ export default function Content({
     recentlyUsedJobCodes,
     recentlyUsedCostCodes,
     recentlyUsedEquipment,
-    ]);
+  ]);
 
-
-  // Calculate total hours for the current pay period
   const totalPayPeriodHours = useMemo(() => {
     return payPeriodSheets
       .filter((sheet): sheet is TimeSheets => sheet.duration !== null)
@@ -122,19 +138,18 @@ export default function Content({
     if (authStep === "success") {
       router.push("/dashboard");
     }
-  }, []);
+  }, [authStep, router]);
 
-  // when you clock out, clear local storage then rests the auth step
   useEffect(() => {
     if (authStep === "removeLocalStorage") {
       localStorage.clear();
       setAuthStep("");
     }
-  }, []);
-
+  }, [authStep]);
 
   useEffect(() => {
     if (session && session.user) {
+      console.log("Session user:", session.user);
       setSavedUserData({
         id: session.user.id,
       });
@@ -153,56 +168,52 @@ export default function Content({
     setToggle(!toggle);
   };
 
-  useEffect(() => {
-    setJobsiteResults(jobCodes);
-    setCostcodeResults(CostCodes);
-    setEquipmentResults(equipment);
-  }, []);
-
-  // Define setHoursContext function
   const setHoursContext = () => {
-    // Set the pay period hours as a string
-    setPayPeriodHours(totalPayPeriodHours.toFixed(2)); // Ensure it's to 2 decimal places
+    setPayPeriodHours(totalPayPeriodHours.toFixed(2));
   };
 
-  // Helper function to capitalize the first letter of a string
   function capitalize(str: any) {
     if (typeof str !== "string") {
-    return "";
+      return "";
     }
     return str.charAt(0).toUpperCase() + str.slice(1);
-    }
+  }
 
   if (authStep === "break") {
     return (
       <>
         <Bases variant={"default"}>
-        <Contents size={"default"}>
-          <Header />
-          <Sections size={"default"}>
           <Contents size={"default"}>
-            <Headers variant={"relative"} size={"default"}></Headers>
-            <Banners variant={"default"}>
-              <Titles variant={"default"} size={"h1"}>
-                {t("Banner")}
-              </Titles>
-              <Texts variant={"default"} size={"p1"}>
-                {t("Date", { date: capitalize(date) })}
-              </Texts>
-            </Banners>
-            <Texts variant={"name"} size={"p1"}>
-              {t("Name", {
-                firstName: capitalize(user.firstName),
-                lastName: capitalize(user.lastName),
-              })}
-            </Texts>
-            <Contents size={"default"}>
-            <DisplayBreakTime setToggle={handler} display={toggle} />
-            <WidgetSection user={user} display={toggle} locale={locale} option={"break"} />
-            </Contents>
-            </Contents>
-          </Sections>
-        </Contents>
+            <Header />
+            <Sections size={"default"}>
+              <Contents size={"default"}>
+                <Headers variant={"relative"} size={"default"}></Headers>
+                <Banners variant={"default"}>
+                  <Titles variant={"default"} size={"h1"}>
+                    {t("Banner")}
+                  </Titles>
+                  <Texts variant={"default"} size={"p1"}>
+                    {t("Date", { date: capitalize(date) })}
+                  </Texts>
+                </Banners>
+                <Texts variant={"name"} size={"p1"}>
+                  {t("Name", {
+                    firstName: capitalize(user.firstName),
+                    lastName: capitalize(user.lastName),
+                  })}
+                </Texts>
+                <Contents size={"default"}>
+                  <DisplayBreakTime setToggle={handler} display={toggle} />
+                  <WidgetSection
+                    user={user}
+                    display={toggle}
+                    locale={locale}
+                    option={"break"}
+                  />
+                </Contents>
+              </Contents>
+            </Sections>
+          </Contents>
         </Bases>
       </>
     );
@@ -212,28 +223,35 @@ export default function Content({
         <Bases variant={"default"}>
           <Header />
           <Contents>
-          <Sections size={"homepage"}>
-          <Contents variant={"header"} size={"test"}>
-            <Headers variant={"relative"} size={"default"}></Headers>
-          </Contents>
-            <Banners variant={"default"}>
-              <Titles variant={"default"} size={"h1"}>{t("Banner")}</Titles>
-              <Texts variant={"default"} size={"p1"}>{t("Date", { date: capitalize(date) })}</Texts>
-            </Banners>
-            <Contents variant={"name"} size={"test"}>
-              <Texts variant={"name"} size={"p1"}>
-                {t("Name", {firstName: capitalize(user.firstName),lastName: capitalize(user.lastName),})}
-              </Texts>
-            </Contents>
-            <Grids variant={"widgets"} size={"default"}>
-              <Hours setToggle={handler} display={toggle} />
-              <WidgetSection user={user} display={toggle} locale={locale} />
-            </Grids>
-            <Footers>{f("Copyright")}</Footers>
-          </Sections>
+            <Sections size={"homepage"}>
+              <Contents variant={"header"} size={"test"}>
+                <Headers variant={"relative"} size={"default"}></Headers>
+              </Contents>
+              <Banners variant={"default"}>
+                <Titles variant={"default"} size={"h1"}>
+                  {t("Banner")}
+                </Titles>
+                <Texts variant={"default"} size={"p1"}>
+                  {t("Date", { date: capitalize(date) })}
+                </Texts>
+              </Banners>
+              <Contents variant={"name"} size={"test"}>
+                <Texts variant={"name"} size={"p1"}>
+                  {t("Name", {
+                    firstName: capitalize(user.firstName),
+                    lastName: capitalize(user.lastName),
+                  })}
+                </Texts>
+              </Contents>
+              <Grids variant={"widgets"} size={"default"}>
+                <Hours setToggle={handler} display={toggle} />
+                <WidgetSection user={user} display={toggle} locale={locale} />
+              </Grids>
+              <Footers>{f("Copyright")}</Footers>
+            </Sections>
           </Contents>
         </Bases>
       </>
     );
-  }  
+  }
 }
