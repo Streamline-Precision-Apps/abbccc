886d83bfe5df3cf34792150645f433374bdb6f6f
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-09-20 13:48:22 -0600

    pushing all new api get routes

---
+++ shift-scan/src/app/api/getAllJobsites/route.tsx
@@ -0,0 +1,17 @@
+"use server";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { auth } from "@/auth";
+
+export async function GET() {
+    const session = await auth();
+    const userId = session?.user.id;
+
+    if (!userId) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const jobsiteData = await prisma.jobsites.findMany({});
+
+return NextResponse.json(jobsiteData);
+}
