249eb225b4757ecd5562523cb5ccad20b5c7fb12
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-10-01 12:14:31 -0600

    save the new translation sections

--- shift-scan/src/app/(routes)/timesheets/page.tsx
+++ shift-scan/src/app/(routes)/timesheets/page.tsx
@@ -6,6 +6,7 @@ import { Contents } from "@/components/(reusable)/contents";
 import { Holds } from "@/components/(reusable)/holds";
 import { TitleBoxes } from "@/components/(reusable)/titleBoxes";
 import { redirect } from "next/navigation";
+import { getTranslations } from "next-intl/server";
 
 export default async function Timesheets() {
     const session = await auth();
@@ -13,13 +14,12 @@ export default async function Timesheets() {
         redirect('/signin');
     }
     const id = session?.user.id;
-    
-    
+    const t = await getTranslations("TimesheetsContent");
     return (
         <Bases>
         <Contents height={"page"}>
             <Holds background={"white"} size={"full"} className="mb-10">
-                <TitleBoxes title={"View Timesheets"} titleImg={"/form.svg"} titleImgAlt={"Timesheets"}  size={"default"} />
+                <TitleBoxes title={`${t("Title")}`} titleImg={"/form.svg"} titleImgAlt={`${t("Title")}`}  size={"default"} />
             </Holds>
         <ViewTimeSheets user={id} />
         </Contents>
