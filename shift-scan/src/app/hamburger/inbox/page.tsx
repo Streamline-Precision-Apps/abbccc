44377aaf220404de6c50137965c262c61663caaf
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-10-03 07:58:43 -0600

    finished the change password section

--- shift-scan/src/app/hamburger/inbox/page.tsx
+++ shift-scan/src/app/hamburger/inbox/page.tsx
@@ -4,40 +4,42 @@ import { auth } from "@/auth";
 import prisma from "@/lib/prisma";
 
 export default async function Inbox() {
-    const session = await auth();
-    if (!session) return null
-    const userId = session?.user.id;
-    const permission = session?.user.permission
+  const session = await auth();
+  if (!session) return null;
+  const userId = session?.user.id;
+  const permission = session?.user.permission;
 
-    // gives you all the inbox of the user with no filter so well come back here later.
-    const sentContent = await prisma.timeoffRequestForms.findMany({
-        where: {
-            employeeId: userId,   
-        }
-    })
+  // gives you all the inbox of the user with no filter so well come back here later.
+  const sentContent = await prisma.timeoffRequestForms.findMany({
+    where: {
+      employeeId: userId,
+    },
+  });
 
-    const myTeamnumber = await prisma.crewMembers.findMany({
-        where: {
-            employeeId: userId,
-        }
-    })
-    const crewMembers = prisma.crewMembers.findMany({
-        where: {
-            crewId: myTeamnumber[0].crewId,
-            supervisor: false
-        }
-        
-    })
+  const myTeamnumber = await prisma.crewMembers.findMany({
+    where: {
+      employeeId: userId,
+    },
+  });
+  const crewMembers = prisma.crewMembers.findMany({
+    where: {
+      crewId: myTeamnumber[0].crewId,
+      supervisor: false,
+    },
+  });
 
-    // get all the time request forms
-    const recievedContent = await prisma.timeoffRequestForms.findMany({
-        where :{
-            status : "PENDING"
-        }
-    })
+  // get all the time request forms
+  const recievedContent = await prisma.timeoffRequestForms.findMany({
+    where: {
+      status: "PENDING",
+    },
+  });
 
-    const pending = sentContent.filter((item) => item.status === "PENDING" );
-        return (
-            <Content sentContent={sentContent} receivedContent={recievedContent} session={session} />
-        )
-    }
+  return (
+    <Content
+      sentContent={sentContent}
+      receivedContent={recievedContent}
+      session={session}
+    />
+  );
+}
