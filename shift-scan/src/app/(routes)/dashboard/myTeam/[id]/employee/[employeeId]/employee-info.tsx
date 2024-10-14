ffc19f729beecef6b2429f0d34a3367eee6d9a27
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-10-14 12:18:23 -0600

    save changes this has not broken. Updated the timesheets and made the equipment logs functional and editable once more. added some dividers for context. added loading state to different parts of the edit section.

--- shift-scan/src/app/(routes)/dashboard/myTeam/[id]/employee/[employeeId]/employee-info.tsx
+++ shift-scan/src/app/(routes)/dashboard/myTeam/[id]/employee/[employeeId]/employee-info.tsx
@@ -8,6 +8,8 @@ import { Inputs } from "@/components/(reusable)/inputs";
 import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
 import { set } from "zod";
+import { Grids } from "@/components/(reusable)/grids";
+import Spinner from "@/components/(animations)/spinner";
 
 export default function employeeInfo({
   params,
@@ -15,63 +17,102 @@ export default function employeeInfo({
   params: { employeeId: string };
 }) {
   const t = useTranslations("MyTeam");
-  const [employee, setEmployee] = useState<any>([]);
-  const [contacts, setContacts] = useState<any>([]);
+  const [employee, setEmployee] = useState<any>({});
+  const [contacts, setContacts] = useState<any>({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetch(`/api/getUserInfo/${params.employeeId}`);
-      const res = await data.json();
-      console.log(res);
-      setEmployee(res);
-      setContacts(res);
-      console.log(contacts);
+      setLoading(true);
+      try {
+        const data = await fetch(`/api/getUserInfo/${params.employeeId}`);
+        const res = await data.json();
+        if (res.error) {
+          console.error(res.error);
+        } else {
+          setEmployee(res);
+          if (res.contacts && res.contacts.length > 0) {
+            setContacts(res.contacts[0]);
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchData();
   }, [params.employeeId]);
 
   return (
-    <Contents>
-      <Holds background={"white"} className="mb-3">
+    <>
+      <Holds background={"white"} className=" mt-7 mb-5">
         <TitleBoxes
-          title={`${employee?.firstName} ${employee?.lastName}`}
-          titleImg={employee?.image ?? "/johnDoe.webp"}
+          title={
+            loading
+              ? "loading..."
+              : `${employee?.firstName} ${employee?.lastName}`
+          }
+          titleImg={employee?.image ?? "/profile-default.svg"}
           titleImgAlt="Team"
-          type="profilePic"
+          type="profilePics"
         />
       </Holds>
-      <Holds background={"white"}>
-        <Contents width={"section"}>
-          <Forms>
-            <Labels>
-              {t("PhoneNumber")}
-              <Inputs state="disabled" data={contacts.phoneNumber}></Inputs>
-            </Labels>
-            <Labels>
-              {t("Email")}
-              <Inputs state="disabled" data={contacts?.email}></Inputs>
-            </Labels>
-            <Labels>
-              {t("EmergencyContact")}
-              <Inputs
-                state="disabled"
-                data={contacts?.emergencyContact}
-              ></Inputs>
-            </Labels>
-            <Labels>
-              {t("EmergencyContactNumber")}
-              <Inputs
-                state="disabled"
-                data={contacts?.emergencyContactNumber}
-              ></Inputs>
-            </Labels>
-            <Labels>
-              {t("DOB")}
-              <Inputs state="disabled" data={employee?.DOB}></Inputs>
-            </Labels>
-          </Forms>
-        </Contents>
-      </Holds>
-    </Contents>
+      {loading ? (
+        <>
+          <Holds background={"white"} className="h-full ">
+            <Contents width={"section"}>
+              <Grids rows={"5"} className="my-5 h-full">
+                <Holds className="h-full row-span-1">
+                  <Holds className="h-[20px] row-span-1"></Holds>
+                  <Spinner />
+                  <Holds className="h-[20px] row-span-1"></Holds>
+                  <Holds className="h-[20px] row-span-1"></Holds>
+                  <Holds className="h-[20px] row-span-1"></Holds>
+                </Holds>
+              </Grids>
+            </Contents>
+          </Holds>
+        </>
+      ) : (
+        <>
+          <Holds background={"white"} className="h-full my-auto">
+            <Contents width={"section"}>
+              <Grids rows={"5"} gap={"5"} className="my-5">
+                <Labels className="row-span-1 h-full">
+                  {t("PhoneNumber")}
+                  <Inputs
+                    state="disabled"
+                    data={contacts?.phoneNumber}
+                  ></Inputs>
+                </Labels>
+                <Labels className="row-span-1 h-full">
+                  {t("Email")}
+                  <Inputs state="disabled" data={contacts?.email}></Inputs>
+                </Labels>
+                <Labels className="row-span-1 h-full">
+                  {t("EmergencyContact")}
+                  <Inputs
+                    state="disabled"
+                    data={contacts?.emergencyContact}
+                  ></Inputs>
+                </Labels>
+                <Labels className="row-span-1 h-full">
+                  {t("EmergencyContactNumber")}
+                  <Inputs
+                    state="disabled"
+                    data={contacts?.emergencyContactNumber}
+                  ></Inputs>
+                </Labels>
+                <Labels className="row-span-1 h-full">
+                  {t("DOB")}
+                  <Inputs state="disabled" data={employee?.DOB}></Inputs>
+                </Labels>
+              </Grids>
+            </Contents>
+          </Holds>
+        </>
+      )}
+    </>
   );
 }
---
+++ shift-scan/src/app/api/getEmployeeEquipmentByDate/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { auth } from "@/auth";
+
+export async function GET(req: Request) {
+  const session = await auth();
+  const userId = session?.user?.id;
+  const url = new URL(req.url);
+  const dateString = url.searchParams.get("date");
+
+  if (!dateString) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  // Convert dateString (in YYYY-MM-DD format) to a Date object
+  const date = new Date(dateString); // Automatically interprets the string as YYYY-MM-DD
+  if (isNaN(date.getTime())) {
+    return NextResponse.json({ error: "Invalid date" }, { status: 400 });
+  }
+
+  const isoDate = date.toISOString(); // Convert to ISO format (this will include time, which can be truncated if needed)
+
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  // Include the date filter with the ISO date (optional: only keep the date part if time is not needed)
+  const equipmentData = await prisma.employeeEquipmentLogs.findMany({
+    where: {
+      date: {
+        gte: new Date(`${dateString}T00:00:00.000Z`), // Start of the day
+        lt: new Date(`${dateString}T23:59:59.999Z`), // End of the day
+      },
+    },
+    include: {
+      Equipment: {
+        select: {
+          id: true,
+          qrId: true,
+          name: true,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(equipmentData);
+}
