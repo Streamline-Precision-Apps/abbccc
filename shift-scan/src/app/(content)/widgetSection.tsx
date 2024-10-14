8194326eb2c5c1e0297acadd77a566a78f00c348
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-10-14 14:40:12 -0600

    save a done for now view timesheets

--- shift-scan/src/app/(content)/widgetSection.tsx
+++ shift-scan/src/app/(content)/widgetSection.tsx
@@ -15,61 +15,58 @@ import { usePayPeriodTimeSheet } from "../context/PayPeriodTimeSheetsContext";
 import { useRouter } from "next/navigation";
 import { Session } from "next-auth";
 type props = {
-    session: Session;
-}
-export default function WidgetSection( {session} : props) {
-const [loading, setLoading] = useState(true);
-const router = useRouter();
-const t = useTranslations("ManagerButtons");
-const f = useTranslations("Home");
-const [toggle, setToggle] = useState(true);
-const handleToggle = () => setToggle(!toggle);
-const authStep = getAuthStep();
-const permission = session.user.permission;
-const accountSetup = session.user.accountSetup;
+  session: Session;
+};
+export default function WidgetSection({ session }: props) {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const f = useTranslations("Home");
+  const w = useTranslations("Widgets");
+  const e = useTranslations("Err-Msg");
+  const [toggle, setToggle] = useState(true);
+  const handleToggle = () => setToggle(!toggle);
+  const authStep = getAuthStep();
+  const permission = session.user.permission;
+  const accountSetup = session.user.accountSetup;
 
-const { setPayPeriodHours } = usePayPeriodHours();
-const { setPayPeriodTimeSheets} = usePayPeriodTimeSheet();
-const [payPeriodSheets, setPayPeriodSheets] = useState([]);
+  const { setPayPeriodHours } = usePayPeriodHours();
+  const { setPayPeriodTimeSheets } = usePayPeriodTimeSheet();
+  const [payPeriodSheets, setPayPeriodSheets] = useState([]);
 
-useEffect(() => {
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch("/api/getPayPeriodTimeSheets");
-      const data = await response.json();
-      setPayPeriodSheets(data);
-      setPayPeriodTimeSheets(data); // Update the context after fetching
-    } catch (error) {
-      console.error("Error fetching pay period sheets:", error);
-    }
-    finally {
-    setLoading(false);
-    }
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("/api/getPayPeriodTimeSheets");
+        const data = await response.json();
+        setPayPeriodSheets(data);
+        setPayPeriodTimeSheets(data); // Update the context after fetching
+      } catch (error) {
+        console.error(e("PayPeriod-Fetch"), error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  fetchData();
-}, [setPayPeriodTimeSheets]);
+    fetchData();
+  }, [setPayPeriodTimeSheets]);
 
-//---------------------------------------------------------------------
- // Redirect to dashboard if authStep is success
-useEffect(() => {
+  //---------------------------------------------------------------------
+  // Redirect to dashboard if authStep is success
+  useEffect(() => {
     if (authStep === "success") {
       router.push("/dashboard");
     }
     if (authStep === "removeLocalStorage") {
-        localStorage.clear();
-        setAuthStep("");
-      }
-    if (!accountSetup)
-    {
-    router.push("/signin/signup");
+      localStorage.clear();
+      setAuthStep("");
     }
-    
+    if (!accountSetup) {
+      router.push("/signin/signup");
+    }
   }, [authStep, router]);
 
-
-//-----------------------------------------------------------------------
+  //-----------------------------------------------------------------------
   // Calculate total pay period hours
   const totalPayPeriodHours = useMemo(() => {
     if (!payPeriodSheets.length) return 0;
@@ -77,124 +74,141 @@ useEffect(() => {
       .filter((sheet: any) => sheet.duration !== null)
       .reduce((total, sheet: any) => total + (sheet.duration ?? 0), 0);
   }, [payPeriodSheets]);
-  
+
   useEffect(() => {
     setPayPeriodHours(totalPayPeriodHours.toFixed(2));
   }, [totalPayPeriodHours, setPayPeriodHours]);
 
-return (
-<Contents width={"section"} className="py-5">
-<Grids rows={"5"} cols={"2"} gap={"5"}>
-{authStep === "break" ? (
-<>
-
-{/* A ternary statement to display the break widget or view hours */}
-    {toggle ? (
-      <Holds className={toggle ? "col-span-2 row-span-1 gap-5 h-full" : "col-span-2 row-span-5 gap-5 h-full"}>
-        <DisplayBreakTime
-        setToggle={handleToggle}
-        display={toggle}/>
-      </Holds>
-    ) : (
-      <Holds className="bg-pink-700 col-span-2 row-span-5 gap-5 h-full">
-        <Hours //----------------------active hour viewer on break
-        setToggle={handleToggle} 
-        display={toggle} 
-        loading={loading}/>
-      </Holds>
-    )}
-    </>
-) :
-(
-    <>
-      <Holds 
-      className={toggle ? "col-span-2 row-span-1 gap-5 h-full" : "col-span-2 row-span-5 gap-5 h-full"}>
-        <Hours //----------------------This is the view hours widget while clocked out
-        setToggle={handleToggle} 
-        display={toggle} 
-        loading={loading}/>
-      </Holds>
-</>
-)
-}
-{toggle ? (
-<>
-    { permission === "ADMIN" || permission === "SUPERADMIN" || permission === "MANAGER"  ? (
-      <>
-    <Holds position={"row"} className="col-span-2 row-span-2 gap-5 h-full">
-      <Buttons //----------------------This is the qr generator widget
-      background={"lightBlue"}
-      href="/dashboard/qr-generator">
-        <Holds>
-          <Images 
-          titleImg="/qr.svg" 
-          titleImgAlt="QR Code"
-          size={"40"}/>
-        </Holds>
-        <Holds>
-          <Texts size={"p3"}>{f("Qr-btn")}</Texts>
-        </Holds>
-      </Buttons>
-      <Buttons //----------------------This is the my team widget
-      background={"lightBlue"}
-      href="/dashboard/myTeam">
-        <Holds>
-          <Images 
-          titleImg="/team.svg" 
-          titleImgAlt="my team" 
-          size={"40"}/>
-        </Holds>
-        <Holds>
-          <Texts size={"p3"}>{f("MyTeam-btn")}</Texts>
-        </Holds>
-      </Buttons> 
-    </Holds>
-    </>
-) :(null)
-}
-{authStep === "break" ? (
-        <Holds className="col-span-2 row-span-2 gap-5 h-full">
-          <Buttons //----------------------This is the clock in widget while on break
-          background={"orange"} 
-          href="/break">
-            <Holds position={"row"} className="my-auto">
-              <Holds size={"70"}>
-                <Texts size={"p1"}>{f("Clock-btn")}</Texts>
+  return (
+    <Contents width={"section"} className="py-5">
+      <Grids rows={"5"} cols={"2"} gap={"5"}>
+        {authStep === "break" ? (
+          <>
+            {/* A ternary statement to display the break widget or view hours */}
+            {toggle ? (
+              <Holds
+                className={
+                  toggle
+                    ? "col-span-2 row-span-1 gap-5 h-full"
+                    : "col-span-2 row-span-5 gap-5 h-full"
+                }
+              >
+                <DisplayBreakTime setToggle={handleToggle} display={toggle} />
               </Holds>
-              <Holds size={"30"}>
-                <Images 
-                titleImg="/clock-in.svg" 
-                titleImgAlt="Clock In Icon" 
-                size={"50"}/>
+            ) : (
+              <Holds className="bg-pink-700 col-span-2 row-span-5 gap-5 h-full">
+                <Hours //----------------------active hour viewer on break
+                  setToggle={handleToggle}
+                  display={toggle}
+                  loading={loading}
+                />
               </Holds>
+            )}
+          </>
+        ) : (
+          <>
+            <Holds
+              className={
+                toggle
+                  ? "col-span-2 row-span-1 gap-5 h-full"
+                  : "col-span-2 row-span-5 gap-5 h-full"
+              }
+            >
+              <Hours //----------------------This is the view hours widget while clocked out
+                setToggle={handleToggle}
+                display={toggle}
+                loading={loading}
+              />
             </Holds>
-          </Buttons>
-        </Holds>
-      ) : (
-        <Holds className="col-span-2 row-span-2 gap-5 h-full">
-          <Buttons //----------------------This is the clock in widget while clocked out
-          background={"green"} 
-          href="/clock">
-            <Holds position={"row"} className="my-auto">
-              <Holds size={"60"}>
-                <Texts size={"p1"}>{f("Clock-btn")}</Texts>
+          </>
+        )}
+        {toggle ? (
+          <>
+            {permission === "ADMIN" ||
+            permission === "SUPERADMIN" ||
+            permission === "MANAGER" ? (
+              <>
+                <Holds
+                  position={"row"}
+                  className="col-span-2 row-span-2 gap-5 h-full"
+                >
+                  <Buttons //----------------------This is the qr generator widget
+                    background={"lightBlue"}
+                    href="/dashboard/qr-generator"
+                  >
+                    <Holds>
+                      <Images
+                        titleImg="/qr.svg"
+                        titleImgAlt="QR Code"
+                        size={"40"}
+                      />
+                    </Holds>
+                    <Holds>
+                      <Texts size={"p3"}>{w("QR")}</Texts>
+                    </Holds>
+                  </Buttons>
+                  <Buttons //----------------------This is the my team widget
+                    background={"lightBlue"}
+                    href="/dashboard/myTeam"
+                  >
+                    <Holds>
+                      <Images
+                        titleImg="/team.svg"
+                        titleImgAlt="my team"
+                        size={"40"}
+                      />
+                    </Holds>
+                    <Holds>
+                      <Texts size={"p3"}>{w("MyTeam")}</Texts>
+                    </Holds>
+                  </Buttons>
+                </Holds>
+              </>
+            ) : null}
+            {authStep === "break" ? (
+              <Holds className="col-span-2 row-span-2 gap-5 h-full">
+                <Buttons //----------------------This is the clock in widget while on break
+                  background={"orange"}
+                  href="/break"
+                >
+                  <Holds position={"row"} className="my-auto">
+                    <Holds size={"70"}>
+                      <Texts size={"p1"}>{f("Clock-btn")}</Texts>
+                    </Holds>
+                    <Holds size={"30"}>
+                      <Images
+                        titleImg="/clock-in.svg"
+                        titleImgAlt="Clock In Icon"
+                        size={"50"}
+                      />
+                    </Holds>
+                  </Holds>
+                </Buttons>
               </Holds>
-              <Holds size={"40"}>
-                <Images 
-                titleImg="/clock-in.svg" 
-                titleImgAlt="Clock In Icon"
-                size={"70"}/>
+            ) : (
+              <Holds className="col-span-2 row-span-2 gap-5 h-full">
+                <Buttons //----------------------This is the clock in widget while clocked out
+                  background={"green"}
+                  href="/clock"
+                >
+                  <Holds position={"row"} className="my-auto">
+                    <Holds size={"60"}>
+                      <Texts size={"p1"}>{f("Clock-btn")}</Texts>
+                    </Holds>
+                    <Holds size={"40"}>
+                      <Images
+                        titleImg="/clock-in.svg"
+                        titleImgAlt="Clock In Icon"
+                        size={"70"}
+                      />
+                    </Holds>
+                  </Holds>
+                </Buttons>
               </Holds>
-            </Holds>
-          </Buttons>
-        </Holds>)
+            )}
+          </>
+        ) : null}
+      </Grids>
+    </Contents>
+  );
 }
-    </>
-
-        ) : ( null)
-      }
-          </Grids>
-      </Contents>
-      
-    );
-  }
--- shift-scan/src/app/(routes)/signin/page.tsx
+++ shift-scan/src/app/(routes)/signin/page.tsx
@@ -6,22 +6,25 @@ import { Images } from "@/components/(reusable)/images";
 import Password from "./password";
 import { cookies } from "next/headers";
 import { Holds } from "@/components/(reusable)/holds";
+import { getTranslations } from "next-intl/server";
 
 export default async function SignInPage() {
   const locale = cookies().get("locale")?.value || "en";
+  const t = await getTranslations("login");
   return (
     <Bases>
       <Contents>
-          <Images 
-          titleImg="/logo.svg" 
-          titleImgAlt="logo" 
-          background="white" 
-          size="40" 
-          className="mb-5 p-3"/>
-          <Holds background={"white"}>
-            <Password locale={locale}/>
-          </Holds>
+        <Images
+          titleImg="/logo.svg"
+          titleImgAlt={`${t("LogoAlt")}`}
+          background="white"
+          size="40"
+          className="mb-5 p-3"
+        />
+        <Holds background={"white"}>
+          <Password locale={locale} />
+        </Holds>
       </Contents>
-    </Bases> 
+    </Bases>
   );
 }
