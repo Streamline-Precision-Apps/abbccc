1d32c1fd1ffc97a6f259b56bbf7f8d7ccf1f6021
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-10-15 12:21:10 -0600

    saving report a bug form

--- shift-scan/src/app/(routes)/dashboard/forms/page.tsx
+++ shift-scan/src/app/(routes)/dashboard/forms/page.tsx
@@ -7,27 +7,40 @@ import { TitleBoxes } from "@/components/(reusable)/titleBoxes";
 import Content from "@/app/(routes)/dashboard/forms/content";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { Grids } from "@/components/(reusable)/grids";
 
 export default async function Forms() {
-    const session = await auth();
-    if (!session) {
-        redirect('/signin');
-    }
-    
-    return (
+  const session = await auth();
+  if (!session) {
+    redirect("/signin");
+  }
+
+  return (
     <Bases>
-    <Contents height="page">
-        <Holds size={"full"} background={"white"}>
+      <Contents>
+        <Grids rows={"5"} gap={"5"} className="mt-7 mb-3">
+          <Holds
+            size={"full"}
+            background={"white"}
+            className="my-auto row-span-1 h-full "
+          >
             <TitleBoxes
-            title="Forms"
-            titleImg="/form.svg"
-            titleImgAlt="Forms"
-            variant="default"
-            size="default"
+              title="Forms"
+              titleImg="/form.svg"
+              titleImgAlt="Forms"
+              variant="default"
+              size="default"
             />
-        </Holds>
-        <Content />
-    </Contents>
+          </Holds>
+          <Holds
+            size={"full"}
+            background={"white"}
+            className="row-span-4 h-full "
+          >
+            <Content />
+          </Holds>
+        </Grids>
+      </Contents>
     </Bases>
-);
+  );
 }
