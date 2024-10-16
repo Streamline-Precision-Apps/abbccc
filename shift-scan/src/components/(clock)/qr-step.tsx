7dd0aa220f869a18a02d2d3079e0c666a7ef41ab
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-10-16 10:36:45 -0600

    made small text changes to the english section under switch jobs to get a better phrase and correct use translations

--- shift-scan/src/components/(clock)/qr-step.tsx
+++ shift-scan/src/components/(clock)/qr-step.tsx
@@ -1,12 +1,12 @@
 "use client";
-import React from 'react';
-import { useTranslations } from 'next-intl';
-import QR from './qr';
-import QR_EQ from './qr-eq';
-import { Buttons } from '../(reusable)/buttons';
-import { Texts } from '../(reusable)/texts';
-import { useRouter } from 'next/navigation';
-import { Titles } from '../(reusable)/titles';
+import React from "react";
+import { useTranslations } from "next-intl";
+import QR from "./qr";
+import QR_EQ from "./qr-eq";
+import { Buttons } from "../(reusable)/buttons";
+import { Texts } from "../(reusable)/texts";
+import { useRouter } from "next/navigation";
+import { Titles } from "../(reusable)/titles";
 
 type QRStepProps = {
   handleAlternativePath: () => void;
@@ -16,37 +16,52 @@ type QRStepProps = {
   type: string;
   url: string;
   option?: string;
-}
+};
 
-export default function QRStep({option, handleReturn, handleAlternativePath, handleNextStep, handleChangeJobsite, type, url }: QRStepProps) {
+export default function QRStep({
+  option,
+  handleReturn,
+  handleAlternativePath,
+  handleNextStep,
+  handleChangeJobsite,
+  type,
+  url,
+}: QRStepProps) {
   const t = useTranslations("Clock");
   const router = useRouter();
   return (
     <>
       {type === "equipment" ? (
-        <Titles variant={"modal"} size={"h1"}>{t('ScanEquipment')}</Titles>
+        <Titles size={"h1"}>{t("ScanEquipment")}</Titles>
       ) : (
-        <Titles variant={"modal"} size={"h1"}>{t('ScanJobSite')}</Titles>
+        <Titles size={"h1"}>{t("ScanJobSite")}</Titles>
       )}
-        <div className="bg-white mx-5 rounded-2xl p-3 border-4 border-black">
-          {type === 'equipment' ? (
-            <QR_EQ handleNextStep={handleNextStep} />
-          ) : (
-            <>
-            <QR handleNextStep={handleNextStep} url={url}  />  
-            </>)}
-        </div>
-        <Buttons onClick={handleAlternativePath}>
-          <Texts size={"p4"}>{t('TroubleScanning')}</Texts>
+      <div className="bg-white mx-5 rounded-2xl p-3 border-4 border-black">
+        {type === "equipment" ? (
+          <QR_EQ handleNextStep={handleNextStep} />
+        ) : (
+          <>
+            <QR handleNextStep={handleNextStep} url={url} />
+          </>
+        )}
+      </div>
+      <Buttons onClick={handleAlternativePath}>
+        {option !== "clockin" && option !== "break" ? (
+          <Texts size={"p4"}>{t("SwitchSites")}</Texts>
+        ) : (
+          <Texts size={"p4"}>{t("TroubleScanning")}</Texts>
+        )}
+      </Buttons>
+      {option === "break" ? (
+        <Buttons onClick={handleReturn} background={"red"}>
+          {t("ReturnToJobsite")}
         </Buttons>
-        {option === "break" ? ( <Buttons onClick={handleReturn}  background={'red'}>
-              {t("ReturnToJobsite")}
-              </Buttons>)
-                : (null)}
-        { type !== 'equipment' && option !== "clockin" &&  option !== "break" && ( <Buttons onClick={handleChangeJobsite} background={'green'} >
-              {t('ChangeJobsite')}
-              </Buttons>)
-        }
+      ) : null}
+      {type !== "equipment" && option !== "clockin" && option !== "break" && (
+        <Buttons onClick={handleChangeJobsite} background={"green"}>
+          {t("ChangeCostCode")}
+        </Buttons>
+      )}
     </>
   );
-};
+}
