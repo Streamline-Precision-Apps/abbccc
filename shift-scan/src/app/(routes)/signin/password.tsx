bccd5ceac708356df39fa50066eb83a4a3c4226e
Zach Robker <Zcrobker@gmail.com>
2024-10-22 13:42:12 -0400

    small changes

--- shift-scan/src/app/(routes)/signin/password.tsx
+++ shift-scan/src/app/(routes)/signin/password.tsx
@@ -14,7 +14,7 @@ import { signIn } from "next-auth/react";
 import { setLocale } from "@/actions/cookieActions";
 import { Forms } from "@/components/(reusable)/forms";
 import { Holds } from "@/components/(reusable)/holds";
-import Checkbox from "@/components/(inputs)/checkbox";
+import Checkbox from "@/components/(inputs)/checkBox";
 import Link from "next/link";
 
 type props = {
