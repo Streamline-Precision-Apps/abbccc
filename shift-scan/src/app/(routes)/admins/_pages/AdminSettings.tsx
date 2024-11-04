964f069a25a58ff188e97e1aa5dc29c050d3cb5d
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-11-04 15:19:05 -0700

    save current place working on making employee info

---
+++ shift-scan/src/app/(routes)/admins/_pages/AdminSettings.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Holds } from "@/components/(reusable)/holds";
+import { Texts } from "@/components/(reusable)/texts";
+import { AdminContact } from "./settings/contact";
+import { AdminEditContact } from "./settings/AdminEditContact";
+import { useState } from "react";
+export const AdminSettings = () => {
+  const [view, setView] = useState(0);
+  const editView = (view: number) => {
+    setView(view);
+  };
+  if (view === 1) {
+    return (
+      <Holds background={"white"} className="h-screen w-full">
+        <Holds position={"row"} className="h-full ">
+          <Holds background={"white"} className=" ">
+            <AdminEditContact editView={editView} />
+          </Holds>
+        </Holds>
+      </Holds>
+    );
+  } else {
+    return (
+      <>
+        <Holds position={"row"} className="h-full gap-5">
+          <Holds className="flex-col h-full w-1/3 hidden lg:flex ">
+            <Holds background={"white"} className="h-1/2 mb-5">
+              <AdminContact editView={editView} />
+            </Holds>
+            <Holds background={"white"} className="h-1/2">
+              <Texts>Settings section here</Texts>
+            </Holds>
+          </Holds>
+          <Holds background={"white"} className="h-full w-full">
+            <Texts>Settings sent reset section here</Texts>
+          </Holds>
+        </Holds>
+      </>
+    );
+  }
+};
