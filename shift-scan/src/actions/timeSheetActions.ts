f4a024970460d44f57e2a70376b88190ac721aaf
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-11-06 21:41:26 -0700

    save good information

--- shift-scan/src/actions/timeSheetActions.ts
+++ shift-scan/src/actions/timeSheetActions.ts
@@ -86,6 +86,11 @@ export async function CreateTimeSheet(formData: FormData) {
     });
 
     console.log("newTimeSheet:", newTimeSheet);
+    revalidatePath("/admins/settings");
+    revalidatePath("/admins/assets");
+    revalidatePath("/admins/reports");
+    revalidatePath("/admins/personnel");
+    revalidatePath("/admins");
     return newTimeSheet;
   } catch (error) {
     console.error("Error creating timesheet:", error);
@@ -301,6 +306,11 @@ export async function updateTimeSheetBySwitch(formData: FormData) {
 
     // Revalidate the path
     revalidatePath(`/`);
+    revalidatePath("/admins/settings");
+    revalidatePath("/admins/assets");
+    revalidatePath("/admins/reports");
+    revalidatePath("/admins/personnel");
+    revalidatePath("/admins");
     return { success: true };
   } catch (error) {
     console.log(error);
