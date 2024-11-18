c9f43c95e26c6193dcebe6f4fc7a58dbda34dd2d
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-11-18 15:50:03 -0700

    save current state of project

--- shift-scan/src/app/api/getAllTimesheetsByDate/[employee]/route.ts
+++ shift-scan/src/app/api/getAllTimesheetsByDate/[employee]/route.ts
@@ -22,7 +22,55 @@ export async function GET(
   }
   const date = new Date(dateQuery);
 
+  const targetDate = new Date(dateQuery);
+  // Lower bound: Start of the day
+  const startOfDay = new Date(
+    Date.UTC(
+      targetDate.getFullYear(),
+      targetDate.getMonth(),
+      targetDate.getDate() + 1,
+      0,
+      0,
+      0,
+      0
+    )
+  );
+
+  // End of the day in UTC
+  const endOfDay = new Date(
+    Date.UTC(
+      targetDate.getFullYear(),
+      targetDate.getMonth(),
+      targetDate.getDate() + 1,
+      23,
+      59,
+      59,
+      999
+    )
+  );
+  console.log("date:", date, "startOfDay:", startOfDay, "endOfDay:", endOfDay);
+
   try {
+    const eqSheets = await prisma.employeeEquipmentLogs.findMany({
+      where: {
+        employeeId: employee,
+        createdAt: {
+          gte: startOfDay.toISOString(), // Start of the day (earlier)
+          lte: endOfDay.toISOString(), // End of the day (later)
+        },
+      },
+      include: {
+        Equipment: {
+          select: {
+            name: true,
+            qrId: true,
+          },
+        },
+      },
+    });
+
+    console.log("eqSheets:", eqSheets[0]?.startTime);
+
     const timesheets = await prisma.timeSheets.findMany({
       where: {
         userId: employee,
@@ -33,7 +81,12 @@ export async function GET(
       orderBy: { date: "desc" },
     });
 
-    return NextResponse.json(timesheets);
+    const combinedResponse = {
+      equipmentLogs: eqSheets,
+      timesheets: timesheets,
+    };
+
+    return NextResponse.json(combinedResponse);
   } catch (error) {
     console.error("Error fetching Time Sheets:", error);
     return NextResponse.json(
