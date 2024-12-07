b146a6afe3be7e53b6beca4098aae4598ab50c02
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2024-12-07 16:55:32 -0700

    save changes for the timesheet extraction. Functions and exports properly.

--- shift-scan/src/app/(routes)/admins/assets/@view/cost-code/[id]/page.tsx
+++ shift-scan/src/app/(routes)/admins/assets/@view/cost-code/[id]/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { ReusableViewLayout } from "@/app/(routes)/admins/personnel/@view/[employee]/_components/reusableViewLayout";
 import { Holds } from "@/components/(reusable)/holds";
-import { Texts } from "@/components/(reusable)/texts";
+
 import { useEffect, useState } from "react";
 import { Inputs } from "@/components/(reusable)/inputs";
 import { EditableFields } from "@/components/(reusable)/EditableField";
+import { Buttons } from "@/components/(reusable)/buttons";
+import { Grids } from "@/components/(reusable)/grids";
+import { Titles } from "@/components/(reusable)/titles";
 
 export default function UpdateCostCodes({
   params,
@@ -19,6 +22,7 @@ export default function UpdateCostCodes({
   const [costcodeId, setCostCodeId] = useState<number>(0);
   const [costcodeName, setCostCodeName] = useState<string>("");
   const [description, setDescription] = useState<string>("");
+  const [changesMade, SetChangesMade] = useState(false);
 
   useEffect(() => {
     const fetchCostcode = async () => {
@@ -33,6 +37,13 @@ export default function UpdateCostCodes({
     };
     fetchCostcode();
   }, [id]);
+
+  useEffect(() => {
+    const hasChanges =
+      costcodeName !== initialCostcodeName ||
+      description !== initialDescription;
+    SetChangesMade(hasChanges);
+  }, [costcodeName, description, initialCostcodeName, initialDescription]);
 
   return (
     <Holds className="w-full h-full ">
@@ -52,7 +63,7 @@ export default function UpdateCostCodes({
         mainHolds="h-full w-full flex flex-row row-span-6 col-span-2 bg-app-dark-blue px-4 py-2 rounded-[10px] gap-4"
         mainLeft={<CostCodeLeft />}
         mainRight={<CostCodeRight />}
-        footer={<EditCostCodeFooter />}
+        footer={<EditCostCodeFooter changesMade={changesMade} />}
       />
     </Holds>
   );
@@ -118,13 +129,30 @@ function EditCostCodeForm({
   );
 }
 
-function EditCostCodeFooter() {
+function EditCostCodeFooter({ changesMade }: { changesMade: boolean }) {
   return (
     <Holds
       background={"white"}
       className="w-full h-full row-span-1 col-span-2 "
     >
-      <Texts>Cost Code Footer</Texts>
+      <Grids cols={"4"} gap={"4"} className="w-full h-full p-4">
+        <Holds className="my-auto col-start-1 col-end-2 ">
+          <Buttons background={"red"} className="py-2 ">
+            <Titles size={"h4"}>Delete Cost Code</Titles>
+          </Buttons>
+        </Holds>
+
+        <Holds className="my-auto col-start-4 col-end-5 ">
+          <Buttons
+            className={
+              "py-2 " + (changesMade ? "bg-app-green" : "bg-slate-400")
+            }
+            disabled={!changesMade}
+          >
+            <Titles size={"h4"}>Submit Edit</Titles>
+          </Buttons>
+        </Holds>
+      </Grids>
     </Holds>
   );
 }
---
+++ shift-scan/src/app/(routes)/admins/reports/@search/default.tsx
@@ -0,0 +1,9 @@
+import { Holds } from "@/components/(reusable)/holds";
+
+export default function ReportDefault() {
+  return (
+    <Holds>
+      <h1>Report Default</h1>
+    </Holds>
+  );
+}
