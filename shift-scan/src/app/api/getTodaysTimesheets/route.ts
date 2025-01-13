b433a84a7dd47504a3acbb0c3f7ab4ee677c40d4
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2025-01-13 15:54:36 -0700

    added a clock out view to be sure that all time cards were reviewed and submitted

---
+++ shift-scan/src/app/api/getTodaysTimesheets/route.ts
@@ -0,0 +1,41 @@
+"use server";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { auth } from "@/auth";
+
+export async function GET() {
+  const session = await auth();
+  const userId = session?.user.id;
+
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  try {
+    // Fetch all timesheets for the current day
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+
+    const endOfDay = new Date();
+    endOfDay.setHours(23, 59, 59, 999);
+
+    const timesheets = await prisma.timeSheet.findMany({
+      where: {
+        userId: userId,
+        date: {
+          gte: startOfDay,
+          lte: endOfDay,
+        },
+      },
+      orderBy: { date: "desc" },
+    });
+
+    return NextResponse.json(timesheets);
+  } catch (error) {
+    console.error("Error fetching Time Sheets:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch pay period sheets" },
+      { status: 500 }
+    );
+  }
+}
