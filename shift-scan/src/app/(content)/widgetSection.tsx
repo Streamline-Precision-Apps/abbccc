1029d3aac8917036edcffdd3fcd4ab556ab58771
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2025-01-13 18:00:44 -0700

    save progress with making the profile section look better

--- shift-scan/src/app/(content)/widgetSection.tsx
+++ shift-scan/src/app/(content)/widgetSection.tsx
@@ -175,12 +175,12 @@ export default function WidgetSection({ session, locale }: Props) {
 
   // Redirect to dashboard if user is an admin
 
-  if (
-    session?.user.permission === "ADMIN" ||
-    (session?.user.permission === "SUPERADMIN" && window.innerWidth >= 820)
-  ) {
-    router.push("/admins");
-  }
+  // if (
+  //   session?.user.permission === "ADMIN" ||
+  //   (session?.user.permission === "SUPERADMIN" && window.innerWidth >= 820)
+  // ) {
+  //   router.push("/admins");
+  // }
 
   return (
     <>
