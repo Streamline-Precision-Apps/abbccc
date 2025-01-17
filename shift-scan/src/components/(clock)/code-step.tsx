5e1de96bed6a8406ff697a2ec8fe4557d5ecfa73
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2025-01-17 14:05:22 -0700

    save fixes from the merge and editing of timesheet

--- shift-scan/src/components/(clock)/code-step.tsx
+++ shift-scan/src/components/(clock)/code-step.tsx
@@ -6,7 +6,6 @@ import { useTranslations } from "next-intl";
 import { Titles } from "../(reusable)/titles";
 import { Grids } from "../(reusable)/grids";
 import { Holds } from "../(reusable)/holds";
-import { Contents } from "../(reusable)/contents";
 
 type CodeStepProps = {
   datatype: string;
@@ -15,27 +14,25 @@ type CodeStepProps = {
 
 export default function CodeStep({ datatype, handleNextStep }: CodeStepProps) {
   const t = useTranslations("Clock");
- 
+
   // TODO: This has an error inside of the browser console.
   return (
-    <Contents width={"section"}>
-      <Holds background={"white"} className="h-screen">
-        <Holds className="h-full w-[90%]">
-          <Grids rows={"7"} gap={"2"} className="my-5 h-full  ">
-            <Holds className="row-span-1">
-              <Titles size={"h1"}>{t(`Title-${datatype}`)}</Titles>
+    <Holds background={"white"} className="h-screen">
+      <Holds className="h-full w-[90%]">
+        <Grids rows={"7"} gap={"2"} className="my-5 h-full  ">
+          <Holds className="row-span-1">
+            <Titles size={"h1"}>{t(`Title-${datatype}`)}</Titles>
+          </Holds>
+          <Holds className="row-span-4 border-[3px] border-black rounded-[10px] h-full">
+            <CodeFinder datatype={datatype} />
+          </Holds>
+          {handleNextStep && (
+            <Holds className="row-span-1 h-full ">
+              <StepButtons handleNextStep={handleNextStep} />
             </Holds>
-            <Holds className="row-span-4 border-[3px] border-black rounded-[10px] h-full">
-              <CodeFinder datatype={datatype} />
-            </Holds>
-            {handleNextStep && (
-              <Holds className="row-span-1 h-full ">
-                <StepButtons handleNextStep={handleNextStep} />
-              </Holds>
-            )}
-          </Grids>
-        </Holds>
+          )}
+        </Grids>
       </Holds>
-    </Contents>
+    </Holds>
   );
 }
