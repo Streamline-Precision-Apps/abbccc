3b8a60562d2b1d4f3402d3679496983ab6de36ab
Devun Fox Durst <122407589+devunfox15@users.noreply.github.com>
2025-02-05 14:42:40 -0700

    saving ui Changes

--- shift-scan/src/app/(routes)/dashboard/clock-out/clockOutContent.tsx
+++ shift-scan/src/app/(routes)/dashboard/clock-out/clockOutContent.tsx
@@ -146,122 +146,134 @@ export default function ClockOutContent() {
   // step 0  is the comment step for clocking out
   if (step === 0) {
     return (
-      <Grids className="grid-rows-1 gap-5">
-        <Holds background={"white"} className="row-span-1 h-full">
-          <Contents width={"section"} className="py-4">
-            <Comment
-              handleClick={handleNextStep}
-              clockInRole={""}
-              setCommentsValue={setCommentsValue}
-              commentsValue={commentsValue}
-            />
-          </Contents>
-        </Holds>
-      </Grids>
+      <Bases>
+        <Contents>
+          <Grids className="grid-rows-1 gap-5">
+            <Holds background={"white"} className="row-span-1 h-full">
+              <Contents width={"section"} className="py-4">
+                <Comment
+                  handleClick={handleNextStep}
+                  clockInRole={""}
+                  setCommentsValue={setCommentsValue}
+                  commentsValue={commentsValue}
+                />
+              </Contents>
+            </Holds>
+          </Grids>
+        </Contents>
+      </Bases>
     );
   }
   if (step === 1) {
     return (
-      <Grids className="grid-rows-1 gap-5">
-        <Holds background={"white"} className="row-span-1 h-full">
-          <Contents width={"section"} className="py-4">
-            <ReviewYourDay handleClick={handleNextStep} />
-          </Contents>
-        </Holds>
-      </Grids>
+      <Bases>
+        <Contents>
+          <Grids className="grid-rows-1 gap-5">
+            <Holds background={"white"} className="row-span-1 h-full">
+              <Contents width={"section"} className="py-4">
+                <ReviewYourDay handleClick={handleNextStep} />
+              </Contents>
+            </Holds>
+          </Grids>
+        </Contents>
+      </Bases>
     );
   }
 
   if (step === 2) {
     return (
-      <>
-        <Holds background={"white"}>
-          <TitleBoxes
-            title={t("InjuryVerification")}
-            titleImg="/end-day.svg"
-            titleImgAlt="Team"
-            href="/dashboard"
-          />
-        </Holds>
-        <Holds background={"white"} className="h-full mt-5 py-4">
-          <Contents width={"section"}>
-            <Grids rows={"8"} className="h-full w-full ">
-              <Holds className="row-start-1 row-end-2">
-                <Texts size={"p3"}>{t("SignBelow")}</Texts>
-              </Holds>
-              <Holds position={"row"} className="row-start-2 row-end-4">
-                <Holds size={"70"}>
-                  <Titles size={"h2"}>{t("SignatureVerify")}</Titles>
+      <Bases>
+        <Contents>
+          <Holds background={"white"}>
+            <TitleBoxes
+              title={t("InjuryVerification")}
+              titleImg="/end-day.svg"
+              titleImgAlt="Team"
+              href="/dashboard"
+            />
+          </Holds>
+          <Holds background={"white"} className="h-full mt-5 py-4">
+            <Contents width={"section"}>
+              <Grids rows={"8"} className="h-full w-full ">
+                <Holds className="row-start-1 row-end-2">
+                  <Texts size={"p3"}>{t("SignBelow")}</Texts>
                 </Holds>
-                <Holds size={"30"}>
-                  <CheckBox
-                    id="injury-checkbox"
-                    name="injury-verify"
-                    onChange={handleCheckboxChange}
-                    defaultChecked={checked}
-                  />
+                <Holds position={"row"} className="row-start-2 row-end-4">
+                  <Holds size={"70"}>
+                    <Titles size={"h2"}>{t("SignatureVerify")}</Titles>
+                  </Holds>
+                  <Holds size={"30"}>
+                    <CheckBox
+                      id="injury-checkbox"
+                      name="injury-verify"
+                      onChange={handleCheckboxChange}
+                      defaultChecked={checked}
+                    />
+                  </Holds>
                 </Holds>
-              </Holds>
-              <Holds className="row-start-4 row-end-6 h-full ">
-                <Holds className="border-[3px] border-black rounded-[10px] h-full">
-                  {loading ? (
-                    <Holds className="my-auto">
-                      <Spinner />
-                    </Holds>
-                  ) : (
-                    <Holds className="my-auto">
-                      {base64String ? (
-                        <Image
-                          src={base64String}
-                          alt="Loading signature"
-                          width={100}
-                          height={100}
-                          className="w-[40%] mx-auto"
-                        />
-                      ) : (
-                        <Holds className="my-auto">
-                          <Texts size={"p2"}>{t("NoSignature")}</Texts>
-                        </Holds>
-                      )}
-                    </Holds>
-                  )}
+                <Holds className="row-start-4 row-end-6 h-full ">
+                  <Holds className="border-[3px] border-black rounded-[10px] h-full">
+                    {loading ? (
+                      <Holds className="my-auto">
+                        <Spinner />
+                      </Holds>
+                    ) : (
+                      <Holds className="my-auto">
+                        {base64String ? (
+                          <Image
+                            src={base64String}
+                            alt="Loading signature"
+                            width={100}
+                            height={100}
+                            className="w-[40%] mx-auto"
+                          />
+                        ) : (
+                          <Holds className="my-auto">
+                            <Texts size={"p2"}>{t("NoSignature")}</Texts>
+                          </Holds>
+                        )}
+                      </Holds>
+                    )}
+                  </Holds>
                 </Holds>
-              </Holds>
 
-              <Holds className="row-start-8 row-end-9 h-full ">
-                <Buttons
-                  background={checked ? "green" : "red"}
-                  onClick={handleNextStepAndSubmit}
-                  disabled={loading}
-                >
-                  <Titles size={"h3"}>
-                    {checked ? t("Continue") : t("ReportInjury")}
-                  </Titles>
-                </Buttons>
-              </Holds>
-            </Grids>
-          </Contents>
-        </Holds>
-      </>
+                <Holds className="row-start-8 row-end-9 h-full ">
+                  <Buttons
+                    background={checked ? "green" : "red"}
+                    onClick={handleNextStepAndSubmit}
+                    disabled={loading}
+                  >
+                    <Titles size={"h3"}>
+                      {checked ? t("Continue") : t("ReportInjury")}
+                    </Titles>
+                  </Buttons>
+                </Holds>
+              </Grids>
+            </Contents>
+          </Holds>
+        </Contents>
+      </Bases>
     );
   } else if (step === 3 && path === "Injury") {
     return (
-      <>
-        <Holds background={"white"}>
-          <TitleBoxes
-            title={t("InjuryVerification")}
-            titleImg="/injury.svg"
-            titleImgAlt={"injury icon"}
-          />
-        </Holds>
-        <Holds background={"white"} className="h-full mt-5 py-4 ">
-          {/* Injury Report Content */}
-          <InjuryReportContent
-            base64String={base64String}
-            handleNextStep={handleSubmitInjury}
-          />
-        </Holds>
-      </>
+      <Bases>
+        <Contents>
+          <Holds background={"white"}>
+            <TitleBoxes
+              title={t("InjuryVerification")}
+              titleImg="/injury.svg"
+              titleImgAlt={"injury icon"}
+            />
+          </Holds>
+          <Holds background={"white"} className="h-full mt-5 py-4 ">
+            {/* Injury Report Content */}
+            <InjuryReportContent
+              base64String={base64String}
+              handleNextStep={handleSubmitInjury}
+            />
+          </Holds>
+        </Contents>
+      </Bases>
     );
   } else if (step === 3 && path === "clockOut") {
     return (
@@ -275,77 +287,79 @@ export default function ClockOutContent() {
     );
   } else if (step === 4 && path === "clockOut") {
     return (
-      <>
-        <Grids rows={"4"} gap={"5"}>
-          <Holds background={"white"} className="row-span-1 h-full">
-            <Contents width={"section"} className="py-4">
-              <TitleBoxes
-                title={t("Bye")}
-                titleImg={"/end-day.svg"}
-                titleImgAlt={"End of Day Icon"}
-              />
-            </Contents>
-          </Holds>
-          <Holds background={"white"} className="row-span-3 h-full">
-            <Contents width={"section"} className="py-5">
-              <Holds background={"lightGray"} className="h-full">
-                <Grids rows={"6"} gap={"5"}>
-                  <Holds className="row-span-1 h-full my-auto">
-                    <Holds position={"right"} size={"20"}>
-                      <Buttons type="button" onClick={handleButtonClick}>
-                        <Images
-                          titleImg={"/downArrow.svg"}
-                          titleImgAlt={"downArrow"}
-                          size={"80"}
-                          className="mx-auto p-2"
-                        />
-                      </Buttons>
+      <Bases>
+        <Contents>
+          <Grids rows={"4"} gap={"5"}>
+            <Holds background={"white"} className="row-span-1 h-full">
+              <Contents width={"section"} className="py-4">
+                <TitleBoxes
+                  title={t("Bye")}
+                  titleImg={"/end-day.svg"}
+                  titleImgAlt={"End of Day Icon"}
+                />
+              </Contents>
+            </Holds>
+            <Holds background={"white"} className="row-span-3 h-full">
+              <Contents width={"section"} className="py-5">
+                <Holds background={"lightGray"} className="h-full">
+                  <Grids rows={"6"} gap={"5"}>
+                    <Holds className="row-span-1 h-full my-auto">
+                      <Holds position={"right"} size={"20"}>
+                        <Buttons type="button" onClick={handleButtonClick}>
+                          <Images
+                            titleImg={"/downArrow.svg"}
+                            titleImgAlt={"downArrow"}
+                            size={"80"}
+                            className="mx-auto p-2"
+                          />
+                        </Buttons>
+                      </Holds>
                     </Holds>
-                  </Holds>
-                  <Holds className="row-span-1 h-full my-auto">
-                    <Texts>
-                      {t("ClockOutDate")} {new Date().toLocaleDateString()}
-                    </Texts>
-                  </Holds>
-                  <Holds className="row-span-1 h-full my-auto">
-                    <Texts>
-                      {t("Jobsite")} {scanResult?.data}
-                    </Texts>
-                  </Holds>
-                  <Holds className="row-span-1 h-full my-auto">
-                    <Texts>
-                      {t("CostCode")} {savedCostCode}
-                    </Texts>
-                  </Holds>
-                  <Holds className="row-span-1 h-full my-auto">
-                    <form ref={formRef}>
-                      <Inputs
-                        type="hidden"
-                        name="endTime"
-                        value={new Date().toISOString()}
-                        readOnly
-                      />
-                      <Inputs
-                        type="hidden"
-                        name="timeSheetComments"
-                        value={""}
-                        readOnly
-                      />
-                      <Buttons
-                        onClick={handleButtonClick}
-                        className="bg-app-red mx-auto flex justify-center w-full h-full py-4 px-5 rounded-lg text-black font-bold mt-5"
-                        disabled={isSubmitting}
-                      >
-                        <Clock time={date.getTime()} />
-                      </Buttons>
-                    </form>
-                  </Holds>
-                </Grids>
-              </Holds>
-            </Contents>
-          </Holds>
-        </Grids>
-      </>
+                    <Holds className="row-span-1 h-full my-auto">
+                      <Texts>
+                        {t("ClockOutDate")} {new Date().toLocaleDateString()}
+                      </Texts>
+                    </Holds>
+                    <Holds className="row-span-1 h-full my-auto">
+                      <Texts>
+                        {t("Jobsite")} {scanResult?.data}
+                      </Texts>
+                    </Holds>
+                    <Holds className="row-span-1 h-full my-auto">
+                      <Texts>
+                        {t("CostCode")} {savedCostCode}
+                      </Texts>
+                    </Holds>
+                    <Holds className="row-span-1 h-full my-auto">
+                      <form ref={formRef}>
+                        <Inputs
+                          type="hidden"
+                          name="endTime"
+                          value={new Date().toISOString()}
+                          readOnly
+                        />
+                        <Inputs
+                          type="hidden"
+                          name="timeSheetComments"
+                          value={""}
+                          readOnly
+                        />
+                        <Buttons
+                          onClick={handleButtonClick}
+                          className="bg-app-red mx-auto flex justify-center w-full h-full py-4 px-5 rounded-lg text-black font-bold mt-5"
+                          disabled={isSubmitting}
+                        >
+                          <Clock time={date.getTime()} />
+                        </Buttons>
+                      </form>
+                    </Holds>
+                  </Grids>
+                </Holds>
+              </Contents>
+            </Holds>
+          </Grids>
+        </Contents>
+      </Bases>
     );
   } else {
     return null;
