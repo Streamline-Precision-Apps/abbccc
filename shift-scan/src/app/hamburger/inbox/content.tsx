c04ca55693210e65719289e2820dba59656f764e
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-03-04 10:24:29 -0700

    saving an update of prisma  form schema

--- shift-scan/src/app/hamburger/inbox/content.tsx
+++ shift-scan/src/app/hamburger/inbox/content.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { Tab } from "@/components/(reusable)/tab";
 import STab from "@/app/hamburger/inbox/sent";
@@ -9,56 +9,57 @@ import { Grids } from "@/components/(reusable)/grids";
 import { NewTab } from "@/components/(reusable)/newTabs";
 import { Titles } from "@/components/(reusable)/titles";
 import FormSelection from "./formSelection";
+import { z } from "zod";
+import { sentContent } from "@/lib/types";
 
 export default function Content({ isManager }: { isManager: boolean }) {
-  const [activeTab, setActiveTab] = useState(1); // change back to one
+  const [activeTab, setActiveTab] = useState(1);
 
   return (
-    <Holds className="h-full">
-      <Grids className="grid-rows-10">
-        <Holds position={"row"} className="row-span-1 gap-1">
-          <NewTab
-            onClick={() => setActiveTab(1)}
-            isActive={activeTab === 1}
-            isComplete={true}
-            titleImage={"/formSelection.svg"}
-            titleImageAlt={""}
-          >
-            <Titles size={"h4"}>Form Selection</Titles>
-          </NewTab>
-          <NewTab
-            onClick={() => setActiveTab(2)}
-            isActive={activeTab === 2}
-            isComplete={true}
-            titleImage={"/submittedForms.svg"}
-            titleImageAlt={""}
-          >
-            <Titles size={"h4"}>Submitted Forms</Titles>
-          </NewTab>
-          {isManager && (
+    <Holds className={`row-span-8 h-full `}>
+      <Holds className="h-full">
+        <Grids className="grid-rows-10">
+          <Holds position={"row"} className="row-span-1 gap-1">
             <NewTab
-              onClick={() => setActiveTab(3)}
-              isActive={activeTab === 3}
+              onClick={() => setActiveTab(1)}
+              isActive={activeTab === 1}
               isComplete={true}
-              titleImage={"/pendingForms.svg"}
+              titleImage={"/formSelection.svg"}
               titleImageAlt={""}
             >
-              <Titles size={"h4"}>Pending Forms</Titles>
+              <Titles size={"h4"}>Form Selection</Titles>
             </NewTab>
-          )}
-        </Holds>
-        {activeTab === 1 && <FormSelection />}
-        {activeTab !== 1 && (
-          <Holds
-            background={"white"}
-            className="rounded-t-none row-span-9 h-full"
-          >
-            {activeTab === 2 && <STab />}
-
-            {isManager && activeTab === 3 && <RTab />}
+            <NewTab
+              onClick={() => setActiveTab(2)}
+              isActive={activeTab === 2}
+              isComplete={true}
+              titleImage={"/submittedForms.svg"}
+              titleImageAlt={""}
+            >
+              <Titles size={"h4"}>Submitted Forms</Titles>
+            </NewTab>
+            {isManager && (
+              <NewTab
+                onClick={() => setActiveTab(3)}
+                isActive={activeTab === 3}
+                isComplete={true}
+                titleImage={"/pendingForms.svg"}
+                titleImageAlt={""}
+              >
+                <Titles size={"h4"}>Pending Forms</Titles>
+              </NewTab>
+            )}
           </Holds>
-        )}
-      </Grids>
+          {activeTab === 1 && <FormSelection />}
+          {activeTab !== 1 && (
+            <>
+              {activeTab === 2 && <STab />}
+
+              {isManager && activeTab === 3 && <RTab />}
+            </>
+          )}
+        </Grids>
+      </Holds>
     </Holds>
   );
 }
