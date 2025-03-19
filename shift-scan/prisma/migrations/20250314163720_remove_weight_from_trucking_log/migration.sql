e89fd2f402ba19bec0cc022123afd039a1ecb412
Sean Walker <wal20042@byui.edu>
2025-03-19 10:43:13 -0600

    Fix some bugs in tasco and for logs.

---
+++ shift-scan/prisma/migrations/20250314163720_remove_weight_from_trucking_log/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `netWeight` on the `TruckingLog` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "TruckingLog" DROP COLUMN "netWeight";
