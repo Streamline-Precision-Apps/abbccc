806bf37579cecc30b15da85a89f2f175d83a4f34
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-05-06 15:42:20 -0500

    save clock in changes for truck

--- shift-scan/src/app/icons/page.tsx
+++ shift-scan/src/app/icons/page.tsx
@@ -9,61 +9,68 @@ import { Texts } from "@/components/(reusable)/texts";
 import { Bases } from "@/components/(reusable)/bases";
 
 export default function HomePage() {
-const imagesDir = path.join(process.cwd(), "public");
-const files = fs.readdirSync(imagesDir);
-const imageList = files.filter((file) =>
-/\.(png|jpe?g|svg|gif|webp)$/i.test(file)
-);
+  const imagesDir = path.join(process.cwd(), "public");
+  const files = fs.readdirSync(imagesDir);
+  const imageList = files.filter((file) =>
+    /\.(png|jpe?g|svg|gif|webp)$/i.test(file)
+  );
 
-return (
-    <Bases size={"scroll"}>
-    <Titles text={"white"}>All Icons</Titles>
-    <Holds className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3    p-3 w-full">
+  return (
+    <Bases>
+      <Titles text={"white"}>All Icons</Titles>
+      <Holds className="w-full h-full overflow-y-auto pb-10">
+        <Holds className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 p-3 w-full">
+          {imageList.map((img, idx) => {
+            const isOld = img.includes(".old");
+            const bgColor = isOld ? "orange" : "white";
+            // Apply background to every other image
 
-    {imageList.map((img, idx) => {
-        const isOld = img.includes(".old");
-        const bgColor = isOld ? "orange" : "white";
-        // Apply background to every other image
-
-        return (
-        <>
-        <Holds background={bgColor as "orange" | "white"} className="h-full rounded-[10px]" key={idx}>
-            <Holds position={"row"} className="flex flex-row items-center justify-center gap-1" key={idx}>
-                <Holds className="h-[100px] w-[100px]">
+            return (
+              <Holds
+                background={bgColor as "orange" | "white"}
+                className="h-full rounded-[10px]"
+                key={idx}
+              >
+                <Holds
+                  position={"row"}
+                  className="flex flex-row items-center justify-center gap-1"
+                  key={idx}
+                >
+                  <Holds className="h-[100px] w-[100px]">
                     <Images
-                    titleImg={`/${img}`}
-                    titleImgAlt={img.replace(/\.[^/.]+$/, "")}
-                    size={"full"}
+                      titleImg={`/${img}`}
+                      titleImgAlt={img.replace(/\.[^/.]+$/, "")}
+                      size={"full"}
                     />
-                </Holds>
-                <Holds className="h-[75px] w-[75px]">
+                  </Holds>
+                  <Holds className="h-[75px] w-[75px]">
                     <Images
-                    titleImg={`/${img}`}
-                    titleImgAlt={img.replace(/\.[^/.]+$/, "")}
-                    size={"full"}
+                      titleImg={`/${img}`}
+                      titleImgAlt={img.replace(/\.[^/.]+$/, "")}
+                      size={"full"}
                     />
-                </Holds>
-                <Holds className="h-[50px] w-[50px] ">
+                  </Holds>
+                  <Holds className="h-[50px] w-[50px] ">
                     <Images
-                    titleImg={`/${img}`}
-                    titleImgAlt={img.replace(/\.[^/.]+$/, "")}
-                    size={"full"}
+                      titleImg={`/${img}`}
+                      titleImgAlt={img.replace(/\.[^/.]+$/, "")}
+                      size={"full"}
                     />
-                </Holds>
-                <Holds className="h-[25px] w-[25px]">
+                  </Holds>
+                  <Holds className="h-[25px] w-[25px]">
                     <Images
-                    titleImg={`/${img}`}
-                    titleImgAlt={img.replace(/\.[^/.]+$/, "")}
-                    size={"full"}
+                      titleImg={`/${img}`}
+                      titleImgAlt={img.replace(/\.[^/.]+$/, "")}
+                      size={"full"}
                     />
+                  </Holds>
                 </Holds>
-            </Holds>
-            <Texts size={"p5"}>{img.replace(/\.[^/.]+$/, "")}</Texts>
+                <Texts size={"p5"}>{img.replace(/\.[^/.]+$/, "")}</Texts>
+              </Holds>
+            );
+          })}
         </Holds>
-        </>
-        );
-    })}
-</Holds>
-</Bases>
-);
+      </Holds>
+    </Bases>
+  );
 }
