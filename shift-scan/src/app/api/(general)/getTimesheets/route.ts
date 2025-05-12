f755dcf74d923cbd563a797c9df71137e07e747f
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-05-12 11:57:44 -0500

    saving timecards updateed info

--- shift-scan/src/app/api/(general)/getTimesheets/route.ts
+++ shift-scan/src/app/api/(general)/getTimesheets/route.ts
@@ -58,6 +58,13 @@ export async function GET(request: Request) {
         },
       },
       orderBy: { date: "desc" },
+      include: {
+        Jobsite: {
+          select: {
+            name: true,
+          },
+        },
+      },
     });
 
     // Return the timesheets if found, or a message if none are found
