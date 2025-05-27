53013a2478c568345d21f0423e8e90c986f5a007
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-05-27 15:23:05 -0500

    save editing in other views

--- shift-scan/src/components/(reusable)/EditableField.tsx
+++ shift-scan/src/components/(reusable)/EditableField.tsx
@@ -1,11 +1,8 @@
-//This file is for creating new reusable components, copy this
-//code and paste it into your new component for a starting point
-//Ctrl F and find "EditableFields" and replace it with your component name
-
+"use client";
+import React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { HTMLAttributes, FC } from "react";
 import { cn } from "@/components/(reusable)/utils";
-import { read } from "fs";
 
 // this extends the capability of HTMLAttributes or the VariantProps that it can hold, specify your props here
 interface EditableFieldsProps
@@ -16,7 +13,9 @@ interface EditableFieldsProps
   checked?: boolean;
   disable?: boolean;
   placeholder?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
   isChanged: boolean;
   onRevert?: () => void;
   iconSrc?: string;
@@ -26,16 +25,19 @@ interface EditableFieldsProps
   pattern?: string;
   name?: string;
   readonly?: boolean;
+  formDatatype?: "input" | "select";
+  options?: { label: string; value: string }[]; // Added options prop for select type
 }
 
 const EditableFieldsVariants = cva(
-  "flex items-center border-[3px] rounded-[10px] overflow-hidden", // Added overflow-hidden
+  "flex items-center rounded-[10px] overflow-hidden", // Added overflow-hidden
   {
     variants: {
       variant: {
-        default: "border-black",
-        danger: "border-red-500",
-        success: "border-green-500",
+        default: "border-black  border-[3px]",
+        danger: "border-red-500  border-[3px]",
+        success: "border-green-500  border-[3px]",
+        edited: "border-app-orange  border-[3px]",
         noFrames: "border-none rounded-none",
       },
       size: {
@@ -70,46 +72,90 @@ const EditableFields: FC<EditableFieldsProps> = ({
   iconAlt = "revert",
   name,
   readonly = false, // Added readonly prop
+  formDatatype = "input", // Added formDatatype prop
+  options = [], // Added options prop for select type
 }) => {
-  return (
-    <div
-      className={cn(
-        EditableFieldsVariants({ variant, size, className }),
-        "w-full"
-      )}
-    >
-      {/* Input container with flex-1 to take available space */}
-      <div className="flex-1 h-full">
-        <input
-          type={type}
-          value={value}
-          name={name}
-          disabled={disable}
-          checked={checked}
-          onChange={onChange}
-          placeholder={placeholder || ""}
-          className="h-full w-full border-none focus:outline-none px-3 bg-transparent disabled:bg-app-gray"
-          minLength={minLength}
-          maxLength={maxLength}
-          pattern={pattern}
-          readOnly={readonly}
-        />
-      </div>
+  if (formDatatype === "input") {
+    return (
+      <div
+        className={cn(
+          EditableFieldsVariants({ variant, size, className }),
+          "w-full"
+        )}
+      >
+        {/* Input container with flex-1 to take available space */}
+        <div className="flex-1 h-full">
+          <input
+            type={type}
+            value={value}
+            name={name}
+            disabled={disable}
+            checked={checked}
+            onChange={onChange}
+            placeholder={placeholder || ""}
+            className="h-full w-full border-none focus:outline-none px-3 bg-transparent disabled:bg-app-gray"
+            minLength={minLength}
+            maxLength={maxLength}
+            pattern={pattern}
+            readOnly={readonly}
+          />
+        </div>
 
-      {/* Revert button - only appears when needed */}
-      {isChanged && onRevert && (
-        <button
-          type="button"
-          className="w-10 h-full flex-shrink-0 flex items-center justify-center   transition-colors"
-          title="Revert changes"
-          onClick={onRevert}
-        >
-          {/* eslint-disable-next-line @next/next/no-img-element*/}
-          <img src={iconSrc} alt={iconAlt} className="w-5 h-5" />
-        </button>
-      )}
-    </div>
-  );
+        {/* Revert button - only appears when needed */}
+        {isChanged && onRevert && (
+          <button
+            type="button"
+            className="w-10 h-full flex-shrink-0 flex items-center justify-center   transition-colors"
+            title="Revert changes"
+            onClick={onRevert}
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element*/}
+            <img src={iconSrc} alt={iconAlt} className="w-5 h-5" />
+          </button>
+        )}
+      </div>
+    );
+  }
+  if (formDatatype === "select") {
+    return (
+      <div
+        className={cn(
+          EditableFieldsVariants({ variant, size, className }),
+          "w-full"
+        )}
+      >
+        {/* Select container with flex-1 to take available space */}
+        <div className="flex-1 h-full">
+          <select
+            value={value}
+            name={name}
+            disabled={disable}
+            onChange={onChange}
+            className="h-full w-full border-none focus:outline-none px-3 bg-transparent disabled:bg-app-gray"
+          >
+            {options.map((option, index) => (
+              <option key={index} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* Revert button - only appears when needed */}
+        {isChanged && onRevert && (
+          <button
+            type="button"
+            className="w-10 h-full flex-shrink-0 flex items-center justify-center   transition-colors"
+            title="Revert changes"
+            onClick={onRevert}
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element*/}
+            <img src={iconSrc} alt={iconAlt} className="w-5 h-5" />
+          </button>
+        )}
+      </div>
+    );
+  }
 };
 
 export { EditableFields, EditableFieldsVariants };
