b287b741d5d8a4c1c2a4e6ffe0132facde024230
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-06-10 09:39:45 -0500

    saving state of toggle and registration hook for state management

--- shift-scan/src/app/(routes)/admins/assets/components/jobsite/components/JobsiteCostCodeGroups.tsx
+++ shift-scan/src/app/(routes)/admins/assets/components/jobsite/components/JobsiteCostCodeGroups.tsx
@@ -7,7 +7,6 @@ import { Jobsite } from "../../../types";
 import { TagSummary } from "../../../types";
 import Spinner from "@/components/(animations)/spinner";
 import { Titles } from "@/components/(reusable)/titles";
-import { useJobsiteRegistrationForm } from "../hooks/useJobsiteRegistrationForm";
 
 interface JobsiteCostCodeGroupsProps {
   formData: {
@@ -15,16 +14,14 @@ interface JobsiteCostCodeGroupsProps {
     [key: string]: any;
   };
   tagSummaries: TagSummary[];
-  onInputChange: (
-    fieldName: string,
-    value:
-      | string
-      | boolean
-      | Array<{
-          id: string;
-          name: string;
-        }>
+  handleCCTagsChange: (
+    tags: Array<{
+      id: string;
+      name: string;
+    }>
   ) => void;
+  isTagSelected: (tagId: string) => boolean;
+  handleTagToggle: (tagId: string, tagName: string) => void;
   changedFields?: Set<string>;
 }
 
@@ -35,55 +32,11 @@ interface JobsiteCostCodeGroupsProps {
 export default function JobsiteCostCodeGroups({
   formData,
   tagSummaries,
-  onInputChange,
+  handleCCTagsChange,
+  isTagSelected,
+  handleTagToggle,
   changedFields,
 }: JobsiteCostCodeGroupsProps) {
-  const { handleCCTagsChange } = useJobsiteRegistrationForm({
-    onSubmit: async () => {},
-  });
-  const [loading, setLoading] = useState(false);
-
-  // Handle selecting/deselecting all tags
-  const allTagsSelected = useMemo(() => {
-    if (!tagSummaries.length || !formData.CCTags) return false;
-    return tagSummaries.every((tag) =>
-      formData.CCTags?.some((ccTag) => ccTag.id === tag.id)
-    );
-  }, [formData.CCTags, tagSummaries]);
-
-  /**
-   * Toggle a single cost code group for the jobsite
-   */
-  const handleTagToggle = (tagId: string, tagName: string) => {
-    const currentTags = formData.CCTags || [];
-    const tagIndex = currentTags.findIndex((tag) => tag.id === tagId);
-    const newTags =
-      tagIndex >= 0
-        ? [
-            ...currentTags.slice(0, tagIndex),
-            ...currentTags.slice(tagIndex + 1),
-          ] // Remove
-        : [...currentTags, { id: tagId, name: tagName }]; // Add
-
-    handleCCTagsChange(newTags);
-  };
-
-  // Is a particular tag selected?
-  const isTagSelected = (tagId: string) => {
-    return formData.CCTags?.some((tag) => tag.id === tagId) || false;
-  };
-
-  // Is the CCTags field changed?
-  const isFieldChanged = changedFields?.has("CCTags") || false;
-
-  if (loading) {
-    return (
-      <Holds className="w-full h-full flex justify-center items-center">
-        <Spinner size={40} />
-      </Holds>
-    );
-  }
-
   return (
     <Holds className="w-full h-full flex flex-col">
       <Holds className="w-full flex-1 overflow-y-auto no-scrollbar">
@@ -94,18 +47,14 @@ export default function JobsiteCostCodeGroups({
               key={tag.id}
               className="w-full h-[40px] mt-4 first:mt-0 gap-2"
             >
-              <Holds
-                background={"lightBlue"}
-                className="w-full cursor-pointer"
-                onClick={() => handleTagToggle(tag.id, tag.name)}
-              >
+              <Holds background={"lightBlue"} className="w-full cursor-pointer">
                 <Titles size="md">{tag.name}</Titles>
               </Holds>
               <Holds className="w-fit h-full justify-center items-center relative">
                 <CheckBox
                   id={`tag-${tag.id}`}
                   name={`tag-${tag.id}`}
-                  checked={formData.CCTags?.some((tag) => tag.id === tag.id)}
+                  checked={isTagSelected(tag.id)}
                   onChange={() => handleTagToggle(tag.id, tag.name)}
                   width={35}
                   height={35}
