8fcc0c7436c7aa5165e90cc4d85ae4bd4a2c2932
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-06-13 11:08:33 -0500

    saving changes for CompleteEquipmentList

--- shift-scan/src/components/(search)/options.ts
+++ shift-scan/src/components/(search)/options.ts
@@ -4,11 +4,11 @@ import {
   useDBCostcode,
   useDBEquipment,
 } from "@/app/context/dbCodeContext";
-import {
-  useRecentDBJobsite,
-  useRecentDBCostcode,
-  useRecentDBEquipment,
-} from "@/app/context/dbRecentCodesContext";
+// import {
+//   useRecentDBJobsite,
+//   useRecentDBCostcode,
+//   useRecentDBEquipment,
+// } from "@/app/context/dbRecentCodesContext";
 import { JobCodes, CostCodes, EquipmentCode } from "@/lib/types";
 
 interface Option {
@@ -21,11 +21,11 @@ export const CostCodeOptions = (
   searchTerm?: string
 ): Option[] => {
   const { jobsiteResults } = useDBJobsite();
-  const { recentlyUsedJobCodes } = useRecentDBJobsite();
+  // const { recentlyUsedJobCodes } = useRecentDBJobsite();
   const { costcodeResults } = useDBCostcode();
-  const { recentlyUsedCostCodes } = useRecentDBCostcode();
+  // const { recentlyUsedCostCodes } = useRecentDBCostcode();
   const { equipmentResults } = useDBEquipment();
-  const { recentlyUsedEquipment } = useRecentDBEquipment();
+  // const { recentlyUsedEquipment } = useRecentDBEquipment();
 
   let options: Option[] = [];
 
@@ -33,13 +33,13 @@ export const CostCodeOptions = (
     case "costcode":
       if (!costcodeResults) throw new Error("costcodeResults is undefined");
 
-      // Get recently used first, remove nulls
-      const recentCostCodes = recentlyUsedCostCodes
-        .filter((costcode) => costcode !== null)
-        .map((costcode: CostCodes) => ({
-          code: costcode.name,
-          label: costcode.name,
-        }));
+      // // Get recently used first, remove nulls
+      // const recentCostCodes = recentlyUsedCostCodes
+      //   .filter((costcode) => costcode !== null)
+      //   .map((costcode: CostCodes) => ({
+      //     code: costcode.name,
+      //     label: costcode.name,
+      //   }));
 
       // Append all cost codes while avoiding duplicates
       const allCostCodes = costcodeResults
@@ -47,23 +47,24 @@ export const CostCodeOptions = (
         .map((costcode: CostCodes) => ({
           code: costcode.name,
           label: costcode.name,
-        }))
-        .filter(
-          (costcode) =>
-            !recentCostCodes.some((recent) => recent.code === costcode.code)
-        );
+        }));
+      // .filter(
+      //   (costcode) =>
+      //     !recentCostCodes.some((recent) => recent.code === costcode.code)
+      // );
 
-      options = [...recentCostCodes, ...allCostCodes];
+      // options = [...recentCostCodes, ...allCostCodes];
+      options = allCostCodes;
       break;
 
     case "jobsite":
       if (!jobsiteResults) throw new Error("jobsiteResults is undefined");
 
       // Recently used jobsites first
-      const recentJobsites = recentlyUsedJobCodes.map((jobcode: JobCodes) => ({
-        code: jobcode.qrId,
-        label: jobcode.name,
-      }));
+      // const recentJobsites = recentlyUsedJobCodes.map((jobcode: JobCodes) => ({
+      //   code: jobcode.qrId,
+      //   label: jobcode.name,
+      // }));
 
       // Append other jobsites while avoiding duplicates
       const allJobsites = jobsiteResults
@@ -71,13 +72,14 @@ export const CostCodeOptions = (
         .map((jobcode: JobCodes) => ({
           code: jobcode.qrId,
           label: jobcode.name,
-        }))
-        .filter(
-          (jobcode) =>
-            !recentJobsites.some((recent) => recent.code === jobcode.code)
-        );
+        }));
+      // .filter(
+      //   (jobcode) =>
+      //     !recentJobsites.some((recent) => recent.code === jobcode.code)
+      // );
 
-      options = [...recentJobsites, ...allJobsites];
+      // options = [...recentJobsites, ...allJobsites];
+      options = allJobsites;
       break;
 
     case "equipment-operator":
@@ -85,25 +87,25 @@ export const CostCodeOptions = (
       if (!equipmentResults) throw new Error("equipmentResults is undefined");
 
       // Recently used equipment first
-      const recentEquipment = recentlyUsedEquipment.map(
-        (equipment: EquipmentCode) => ({
-          code: equipment.qrId,
-          label: equipment.name,
-        })
-      );
+      // const recentEquipment = recentlyUsedEquipment.map(
+      //   (equipment: EquipmentCode) => ({
+      //     code: equipment.qrId,
+      //     label: equipment.name,
+      //   })
+      // );
 
       // Append other equipment while avoiding duplicates
-      const allEquipment = equipmentResults
-        .map((equipment: EquipmentCode) => ({
-          code: equipment.qrId,
-          label: equipment.name,
-        }))
-        .filter(
-          (equipment) =>
-            !recentEquipment.some((recent) => recent.code === equipment.code)
-        );
+      const allEquipment = equipmentResults.map((equipment: EquipmentCode) => ({
+        code: equipment.qrId,
+        label: equipment.name,
+      }));
+      // .filter(
+      //   (equipment) =>
+      //     !recentEquipment.some((recent) => recent.code === equipment.code)
+      // );
 
-      options = [...recentEquipment, ...allEquipment];
+      // options = [...recentEquipment, ...allEquipment];
+      options = allEquipment;
       break;
 
     default:
