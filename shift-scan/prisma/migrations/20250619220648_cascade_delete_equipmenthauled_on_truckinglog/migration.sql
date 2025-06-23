08a3f2117dd07d3e75ec5703a363a1229d707e40
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-06-23 10:45:41 -0500

    updateing migration

--- shift-scan/prisma/migrations/20250619220648_cascade_delete_equipmenthauled_on_truckinglog/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "EquipmentHauled" DROP CONSTRAINT "EquipmentHauled_truckingLogId_fkey";
-
--- AddForeignKey
-ALTER TABLE "EquipmentHauled" ADD CONSTRAINT "EquipmentHauled_truckingLogId_fkey" FOREIGN KEY ("truckingLogId") REFERENCES "TruckingLog"("id") ON DELETE CASCADE ON UPDATE CASCADE;
