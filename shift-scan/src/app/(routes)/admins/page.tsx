a77476f053ce328c167d0072bacdd4f2eed770ed
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-07-01 15:33:08 -0500

    save details

--- shift-scan/src/app/(routes)/admins/page.tsx
+++ shift-scan/src/app/(routes)/admins/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 import "@/app/globals.css";
 import { Holds } from "@/components/(reusable)/holds";
-import AdminHome from "./_pages/admin-home";
-import { NotificationComponent } from "@/components/(inputs)/NotificationComponent";
 
 // change back to async later
 export default function Admins() {
   return (
     <Holds className="h-full w-full">
-      <NotificationComponent />
-      <AdminHome />
+      <p className="text-xl text-white">Dashboard</p>
     </Holds>
   );
 }
