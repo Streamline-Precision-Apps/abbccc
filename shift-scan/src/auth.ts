6305491bef8db224c388ccfe9537bcc7a3e4be3b
Sean Walker <wal20042@byui.edu>
2025-07-11 11:43:08 -0600

    commits from some changes

--- shift-scan/src/auth.ts
+++ shift-scan/src/auth.ts
@@ -62,19 +62,10 @@ const providers: Provider[] = [
         throw new InvalidLoginError();
       }
 
-      const isValidPassword = () => {
-        if (userId.password === passwords) {
-          return true;
-        } else {
-          return false;
-        }
-      };
-
-      // const isValidPassword = await bcrypt.compare(passwords, user.password);
-      // if (!isValidPassword) {
-      //   console.log("Invalid password");
-      //   throw new InvalidLoginError();
-      // }
+      // Use bcrypt to compare the input password with the stored hash
+      // Import bcryptjs at the top if not already
+      const bcrypt = require('bcryptjs');
+      const isValidPassword = await bcrypt.compare(passwords, userId.password);
 
       if (!isValidPassword) {
         console.log("Invalid password");
