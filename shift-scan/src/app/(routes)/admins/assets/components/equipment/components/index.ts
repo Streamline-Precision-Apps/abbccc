b1e48bbd97c6a887cd7f86905624ec6f8101cbba
Sean Walker <wal20042@byui.edu>
2025-07-15 17:18:15 -0600

    Delete admin section for build

--- shift-scan/src/app/(routes)/admins/assets/components/equipment/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as EquipmentHeaderActions } from "./EquipmentHeaderActions";
-export { default as EquipmentBasicFields } from "./EquipmentBasicFields";
-export { default as VehicleInformationFields } from "./VehicleInformationFields";
-export { default as EquipmentDescriptionFields } from "./EquipmentDescriptionFields";
-export { default as EquipmentFormView } from "./EquipmentFormView";
-export { default as EquipmentEmptyState } from "./EquipmentEmptyState";
-export { default as EquipmentRegistrationView } from "./EquipmentRegistrationView";
