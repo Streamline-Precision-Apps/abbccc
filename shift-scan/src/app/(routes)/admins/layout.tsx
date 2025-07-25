55fcf5d21ae5aba729f5a74499b95b773f3d7b55
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-07-25 12:50:06 -0600

    save breaking admin feature for mobile app caused, adding signout feature to the app

--- shift-scan/src/app/(routes)/admins/layout.tsx
+++ shift-scan/src/app/(routes)/admins/layout.tsx
@@ -6,12 +6,42 @@ import { Toaster } from "@/components/ui/sonner";
 import LeftSidebar from "./_pages/leftSide";
 
 import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import Spinner from "@/components/(animations)/spinner";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  const [notAuthorized, setNotAuthorized] = useState(false);
+
+  useEffect(() => {
+    if (status === "loading") return;
+    const permission = session?.user?.permission;
+    if (permission !== "ADMIN" && permission !== "SUPERADMIN") {
+      setNotAuthorized(true);
+    }
+  }, [session, status, router]);
+
+  if (status === "loading") {
+    return <Spinner />;
+  }
+
+  if (notAuthorized) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold text-white">
+          You are not authorized to view these pages.
+        </h1>
+      </div>
+    );
+  }
+
   return (
     <>
       <Toaster position="top-right" richColors closeButton duration={3000} />
