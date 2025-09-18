2bf25cb7bafce953a58cc3321e4c5fa9c3f74bee
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-09-18 11:16:22 -0600

    update connection to sentry and new dashboard

--- shift-scan/src/instrumentation-client.ts
+++ shift-scan/src/instrumentation-client.ts
@@ -5,15 +5,15 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://b180141309e6a91edc6d620f9e7c0511@o4509549416284160.ingest.us.sentry.io/4509549641662464",
+  dsn: "https://9077502936b7ff3107eb72d86f93e00a@o4510041612419072.ingest.us.sentry.io/4510041616613376",
 
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
 
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
