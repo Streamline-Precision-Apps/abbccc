b3ab39280561acf3488c52ab01ef0d8acb8b6fca
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-09-18 12:07:08 -0600

    loading in saved comments to the pages

--- shift-scan/src/app/(routes)/dashboard/clock-out/page.tsx
+++ shift-scan/src/app/(routes)/dashboard/clock-out/page.tsx
@@ -3,17 +3,26 @@ import { auth } from "@/auth";
 // import ClockOutContent from "@/app/(routes)/dashboard/clock-out/clockOutContent";
 import { redirect } from "next/navigation";
 import TempClockOutContent from "./tempClockOutContent";
+import { ClockOutComment } from "@/actions/timeSheetActions";
 
 export default async function ClockOutPage() {
   const session = await auth();
   if (!session) {
     redirect("/signin");
   }
+
+  const user = session.user;
+
   // Pass user id and permission so downstream logic can determine manager/team status
+  // by user id finds the timecard comment if it exists for switch jobs
+  const clockOutComment = await ClockOutComment({ userId: user.id });
+  console.log("ClockOutComment fetched:", clockOutComment);
+
   return (
     <TempClockOutContent
       userId={session.user.id}
       permission={session.user.permission}
+      clockOutComment={clockOutComment}
     />
   );
 }
