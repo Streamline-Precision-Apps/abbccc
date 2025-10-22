0924d9f7bf835ab671200816dd2c23be5f607653
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-10-22 16:31:23 -0600

    save merging

--- shift-scan/prisma/generated/prisma/index.js
+++ shift-scan/prisma/generated/prisma/index.js
@@ -808,7 +808,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\SBwal\\Documents\\git\\abbccc\\shift-scan\\prisma\\generated\\prisma",
+      "value": "/Users/devunfox/abbccc/shift-scan/prisma/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -817,12 +817,12 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "darwin-arm64",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\SBwal\\Documents\\git\\abbccc\\shift-scan\\prisma\\schema\\schema.prisma",
+    "sourceFilePath": "/Users/devunfox/abbccc/shift-scan/prisma/schema/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -885,8 +885,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "query_engine-windows.dll.node");
-path.join(process.cwd(), "prisma/generated/prisma/query_engine-windows.dll.node")
+path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node");
+path.join(process.cwd(), "prisma/generated/prisma/libquery_engine-darwin-arm64.dylib.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
 path.join(process.cwd(), "prisma/generated/prisma/schema.prisma")
